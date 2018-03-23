3ec45c6b8cd0d63999e61d0da5e6ac38b517c720
Tpp <307926882>
2018-03-23 14:36:49 +0800

    new branch modify mixLayer

--- src/components/adi/mod/web/mixLayer/mixLayer.styles.js
+++ src/components/adi/mod/web/mixLayer/mixLayer.styles.js
@@ -3,34 +3,48 @@ export default [
   {
     data: {
       // 定义mod根div的样式
-      root: {
-        position: 'relative',
-        overflow: 'hidden',
-        zoom: 1
-      },
+      root: {},
       media: {
         width: '100%',
-        height: '100%',
-        position: 'absolute',
-        left: 0,
-        top: 0,
-        'z-index': -1
+        height: '694px'
       },
-      group: {
-        width: '50%',
-        float: 'left',
-        'margin-left': '5px'
+      colEmptyDiv: {
+        width: '0.1px',
+        height: '0.1px'
+      },
+      colPhoto: {
+        width: '100%',
+        float: 'left'
+      },
+      colCouple: {
+        'min-width': '200px',
+        'margin-top': '-500px',
+        display: 'flex',
+        'align-items': 'center',
+        float: 'left'
       }
+    },
+    props: {
+      colPhoto: { span: 10 },
+      colCouple: { span: 16 },
+      rootRow: { gutter: 100 }
     },
     theme: {
       root: ['font_0', 'color_7'],
       paragraph: ['color_3']
     },
     options: {
-      theme: {},
+      theme: {
+        businessName: {
+          fontSize: 'font_5',
+          fontColor: 'color_3'
+        },
+        tagline: {
+          fontSize: 'font_2',
+          fontColor: 'color_2'
+        }
+      },
       config: {}
     }
-  },
-  // style2
-  {}
+  }
 ]
---
+++ src/components/adi/mod/web/mixLayer/mixLayer.templates.js
@@ -0,0 +1,24 @@
+export default [
+  // template 1
+  [
+    { colPhoto: 'media' },
+    { colEmptyDiv: 'photo' },
+    {
+      colCouple: [
+        { colBusinessName: 'businessName' },
+        { colTagline: 'tagline' },
+        { colParagraph: 'paragraph' }
+      ]
+    }
+  ],
+  [
+    { colMedia: 'media' },
+    {
+      colCouple: [
+        { colBusinessName: 'businessName' },
+        { colTagline: 'tagline' },
+        { colParagraph: 'paragraph' }
+      ]
+    }
+  ]
+]
