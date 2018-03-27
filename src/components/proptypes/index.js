a184ebb744e13e8a4f0a7965d6354e586cc4e7e1
ZhangKaitlyn <1873070425@qq.com>
2018-03-27 16:30:44 +0800

    board card

--- src/components/proptypes/index.js
+++ src/components/proptypes/index.js
@@ -1,11 +1,13 @@
 import input from './InputType'
 import link from './LinkType'
 import menu from './MenuType'
+import board from './BoardType'
 import autoSizeInput from './AutoSizeInputType'
 
 export default {
   input,
   link,
   menu,
+  board,
   autoSizeInput
 }
