8807bc710e07c59e7567ec6aecc108eaea4c5828
Rango Yuan <yuanoook@foxmail.com>
2018-03-28 14:22:46 +0800

    fix filemanager tree view

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  KEEPWORK_API_PREFIX: '"http://front.keepwork.com/api/wiki/models"',
-  GITLAB_API_PREFIX: '"http://git.stage.keepwork.com"'
+  KEEPWORK_API_PREFIX: '"http://release.keepwork.com/api/wiki/models"',
+  GITLAB_API_PREFIX: '"http://git.release.keepwork.com"'
 }
