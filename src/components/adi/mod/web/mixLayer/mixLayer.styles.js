9c2fb9e6f7cbdd05d172eedc1dd79a9d26cdb957
Tpp <307926882>
2018-03-28 11:17:00 +0800

    add 9 mixLayer mod style

--- src/components/adi/mod/web/mixLayer/mixLayer.styles.js
+++ src/components/adi/mod/web/mixLayer/mixLayer.styles.js
@@ -10,22 +10,366 @@ export default [
       },
       media: {
         width: '100%',
-        height: '450px'
+        height: '689px'
       },
       colGroup: {
         width: '100%'
       },
       colCouple: {
-        'min-width': '200px',
-        display: 'flex',
-        'margin-top': '-400px',
-        'align-items': 'center'
+        'margin-top': '-550px',
+        'margin-left': '15%'
       }
     },
     props: {
-      colPhoto: { span: 10 },
-      colCouple: { span: 16 },
-      rootRow: { gutter: 100 }
+      colGroup: { span: 10 },
+      colCouple: { span: 10 }
+    },
+    theme: {
+      root: ['font_0', 'color_7'],
+      paragraph: ['color_3']
+    },
+    options: {
+      theme: {
+        businessName: {
+          fontSize: 'font_5',
+          fontColor: 'color_3'
+        },
+        tagline: {
+          fontSize: 'font_2',
+          fontColor: 'color_2'
+        }
+      },
+      config: {}
+    }
+  },
+  // style2
+  {
+    data: {
+      root: {
+        width: '100%',
+        height: '100%',
+        overflow: 'hidden'
+      },
+      media: {
+        width: '100%',
+        height: '689px'
+      },
+      colGroup: {
+        width: '100%'
+      },
+      colCouple: {
+        'margin-top': '-550px',
+        'text-align': 'center',
+        width: '100%'
+      }
+    },
+    props: {
+      colGroup: { span: 10 },
+      colCouple: { span: 10 }
+    },
+    theme: {
+      root: ['font_0', 'color_7'],
+      paragraph: ['color_3']
+    },
+    options: {
+      theme: {
+        businessName: {
+          fontSize: 'font_5',
+          fontColor: 'color_3'
+        },
+        tagline: {
+          fontSize: 'font_2',
+          fontColor: 'color_2'
+        }
+      },
+      config: {}
+    }
+  },
+  // style3
+  {
+    data: {
+      root: {
+        width: '100%',
+        height: '100%',
+        overflow: 'hidden'
+      },
+      media: {
+        width: '100%',
+        height: '689px'
+      },
+      colGroup: {
+        width: '100%'
+      },
+      colCouple: {
+        width: '100%',
+        'margin-top': '-550px',
+        'padding-right': '15%',
+        'text-align': 'right'
+      }
+    },
+    props: {
+      colGroup: { span: 10 },
+      colCouple: { span: 10 }
+    },
+    theme: {
+      root: ['font_0', 'color_7'],
+      paragraph: ['color_3']
+    },
+    options: {
+      theme: {
+        businessName: {
+          fontSize: 'font_5',
+          fontColor: 'color_3'
+        },
+        tagline: {
+          fontSize: 'font_2',
+          fontColor: 'color_2'
+        }
+      },
+      config: {}
+    }
+  },
+  // style4
+  {
+    data: {
+      root: {
+        width: '100%',
+        height: '100%',
+        overflow: 'hidden'
+      },
+      media: {
+        width: '100%',
+        height: '689px'
+      },
+      colGroup: {
+        width: '100%'
+      },
+      colCouple: {
+        width: '100%',
+        'margin-top': '-400px',
+        'margin-left': '15%'
+      }
+    },
+    props: {
+      colGroup: { span: 10 },
+      colCouple: { span: 10 }
+    },
+    theme: {
+      root: ['font_0', 'color_7'],
+      paragraph: ['color_3']
+    },
+    options: {
+      theme: {
+        businessName: {
+          fontSize: 'font_5',
+          fontColor: 'color_3'
+        },
+        tagline: {
+          fontSize: 'font_2',
+          fontColor: 'color_2'
+        }
+      },
+      config: {}
+    }
+  },
+  // style5
+  {
+    data: {
+      root: {
+        width: '100%',
+        height: '100%',
+        overflow: 'hidden'
+      },
+      media: {
+        width: '100%',
+        height: '689px'
+      },
+      colGroup: {
+        width: '100%'
+      },
+      colCouple: {
+        width: '100%',
+        'margin-top': '-400px',
+        'text-align': 'center'
+      }
+    },
+    props: {
+      colGroup: { span: 10 },
+      colCouple: { span: 10 }
+    },
+    theme: {
+      root: ['font_0', 'color_7'],
+      paragraph: ['color_3']
+    },
+    options: {
+      theme: {
+        businessName: {
+          fontSize: 'font_5',
+          fontColor: 'color_3'
+        },
+        tagline: {
+          fontSize: 'font_2',
+          fontColor: 'color_2'
+        }
+      },
+      config: {}
+    }
+  },
+  // style6
+  {
+    data: {
+      root: {
+        width: '100%',
+        height: '100%',
+        overflow: 'hidden'
+      },
+      media: {
+        width: '100%',
+        height: '689px'
+      },
+      colGroup: {
+        width: '100%'
+      },
+      colCouple: {
+        width: '100%',
+        'margin-top': '-400px',
+        'padding-right': '15%',
+        'text-align': 'right'
+      }
+    },
+    props: {
+      colGroup: { span: 10 },
+      colCouple: { span: 10 }
+    },
+    theme: {
+      root: ['font_0', 'color_7'],
+      paragraph: ['color_3']
+    },
+    options: {
+      theme: {
+        businessName: {
+          fontSize: 'font_5',
+          fontColor: 'color_3'
+        },
+        tagline: {
+          fontSize: 'font_2',
+          fontColor: 'color_2'
+        }
+      },
+      config: {}
+    }
+  },
+  // style7
+  {
+    data: {
+      root: {
+        width: '100%',
+        height: '100%',
+        overflow: 'hidden'
+      },
+      media: {
+        width: '100%',
+        height: '689px'
+      },
+      colGroup: {
+        width: '100%'
+      },
+      colCouple: {
+        width: '100%',
+        'margin-top': '-250px',
+        'margin-left': '15%'
+      }
+    },
+    props: {
+      colGroup: { span: 10 },
+      colCouple: { span: 10 }
+    },
+    theme: {
+      root: ['font_0', 'color_7'],
+      paragraph: ['color_3']
+    },
+    options: {
+      theme: {
+        businessName: {
+          fontSize: 'font_5',
+          fontColor: 'color_3'
+        },
+        tagline: {
+          fontSize: 'font_2',
+          fontColor: 'color_2'
+        }
+      },
+      config: {}
+    }
+  },
+  // style8
+  {
+    data: {
+      root: {
+        width: '100%',
+        height: '100%',
+        overflow: 'hidden'
+      },
+      media: {
+        width: '100%',
+        height: '689px'
+      },
+      colGroup: {
+        width: '100%'
+      },
+      colCouple: {
+        width: '100%',
+        'margin-top': '-250px',
+        'text-align': 'center'
+      }
+    },
+    props: {
+      colGroup: { span: 10 },
+      colCouple: { span: 10 }
+    },
+    theme: {
+      root: ['font_0', 'color_7'],
+      paragraph: ['color_3']
+    },
+    options: {
+      theme: {
+        businessName: {
+          fontSize: 'font_5',
+          fontColor: 'color_3'
+        },
+        tagline: {
+          fontSize: 'font_2',
+          fontColor: 'color_2'
+        }
+      },
+      config: {}
+    }
+  },
+  // style9
+  {
+    data: {
+      root: {
+        width: '100%',
+        height: '100%',
+        overflow: 'hidden'
+      },
+      media: {
+        width: '100%',
+        height: '689px'
+      },
+      colGroup: {
+        width: '100%'
+      },
+      colCouple: {
+        width: '100%',
+        'margin-top': '-250px',
+        'padding-right': '15%',
+        'text-align': 'right'
+      }
+    },
+    props: {
+      colGroup: { span: 10 },
+      colCouple: { span: 10 }
     },
     theme: {
       root: ['font_0', 'color_7'],
