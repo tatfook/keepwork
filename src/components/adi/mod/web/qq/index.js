423cc8aec8d98494b2104080305d51aa6d321884
Jan <1720370638@qq.com>
2018-04-03 16:41:35 +0800

    add markdown file of mixPosition and qq, add mixPosition and qq cover, add qq mod style

--- src/components/adi/mod/web/qq/index.js
+++ src/components/adi/mod/web/qq/index.js
@@ -1,7 +1,8 @@
 import { generateProperties } from '@/components/adi/mod/base/base.utils'
-import mod from '@/components/adi/mod/base/Base'
+// import mod from '@/components/adi/mod/base/Base'
 import styles from './qq.styles'
 import templates from './qq.templates'
+import mod from './QQ'
 
 const name = 'ModQQ'
 
@@ -15,6 +16,7 @@ const properties = generateProperties(name, components)
 properties.pic.src = './static/adi/qq/qqMod.png'
 properties.pic.link =
   'http://wpa.qq.com/msgrd?v=3&uin=123456789&site=qq&menu=yes'
+
 properties.desc.name = '客服中心'
 
 export default { mod, name, components, properties, styles, templates }
