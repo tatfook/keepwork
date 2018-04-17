a8062a17e263baf42d60647bc0accc0160bd5dcf
onedou <onedou@126.com>
2018-04-17 17:47:18 +0800

    1. comment comp updated 2. mix layer style updated

---
+++ src/components/adi/mod/web/mixLayer/mixLayer.vue
@@ -0,0 +1,13 @@
+<script>
+import _ from 'lodash'
+import baseMixin from '../../base/base.mixin'
+
+export default {
+  mixins: [baseMixin],
+  created() {
+    this.conf.styles[0].data['@media only screen and (max-width: 768px)'].title = {
+      color: this.themeData('color_4')
+    }
+  }
+}
+</script>
