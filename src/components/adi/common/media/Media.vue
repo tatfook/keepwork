00426c934898c607f3497be05f4f33f722a1d080
onedou <onedou@126.com>
2018-04-17 14:59:14 +0800

    1. media comp updated 2. title mod udpated 3. text mod updated

--- src/components/adi/common/media/Media.vue
+++ src/components/adi/common/media/Media.vue
@@ -1,8 +1,9 @@
 <template>
   <div class='comp-media'>
     <a :href='link'>
-      <div v-if='isImage' class="img" :style="loadImg"></div>
+      <div class="img" v-if='isImage' :style="loadImg"></div>
       <video v-else-if='isVideo' :src='src'></video>
+      <div class="svg" v-html="svg()" :style="svgFill"></div>
     </a>
   </div>
 </template>
@@ -10,31 +11,60 @@
 <script>
 import Media from './media.types'
 import compBaseMixin from '../comp.base.mixin'
+import { Base64 } from 'js-base64';
 
 export default {
   name: 'AdiMedia',
   mixins: [compBaseMixin],
+  methods: {
+    svg() {
+      if(this.isBase64Svg) {
+        let base64Svg = this.src.split(',')[1] ? this.src.split(',')[1] : '';
+
+        return Base64.decode(base64Svg)
+      }
+    }
+  },
   computed: {
     isImage() {
-      return Media.isImage(this.properties.src)
+      return Media.isImage(this.src)
     },
     isVideo() {
-      return Media.isVideo(this.properties.src)
+      return Media.isVideo(this.src)
+    },
+    isBase64Svg() {
+      return Media.isBase64Svg(this.src)
     },
     src() {
       return this.properties.src ? this.properties.src : this.options.emptySrc
     },
     link() {
-      return this.properties.link
+      return this.properties.link ? this.properties.link : this.options.emptyLink
     },
     loadImg() {
       return this.generateStyleString({
         'background-image': 'url(' + this.src + ')'
       })
+    },
+    svgFill() {
+      return this.generateStyleString({
+        'fill': this.options.svgFillColor
+      })
     }
   }
 }
 </script>
+
+<style lang="scss">
+.comp-media {
+  .svg {
+    svg {
+      width: 100%;
+      height:100%;
+    }
+  }
+}
+</style>
 
 <style lang="scss" scoped>
 .comp-media {
