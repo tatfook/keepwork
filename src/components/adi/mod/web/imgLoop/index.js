cb960d44aa4bbf68e812c2ced54ac3e60a570f82
Tpp <307926882>
2018-04-19 14:42:23 +0800

    add mixLayer/imgLoop/paracraft/text/title mod explain

--- src/components/adi/mod/web/imgLoop/index.js
+++ src/components/adi/mod/web/imgLoop/index.js
@@ -1,3 +1,30 @@
+/*
+---
+title: imgLoop Mod
+name: imgLoop Mod
+category: Adi Mod
+---
+## @ImgLoop
+
+## 参数说明
+
+```
+- styleID :  //样式ID
+
+# imgLoop
+
+# imgLoop.data
+
+# imgLoop.data.0
+- img : // 轮播图1 图片地址
+- link : // 轮播图1 图片链接
+
+# imgLoop.data.1
+- img : // 轮播图2 图片地址
+- link : // 轮播图2 图片链接
+```
+*/
+
 import { generateProperties } from '@/components/adi/mod/base/base.utils'
 import mod from '@/components/adi/mod/base/Base'
 import styles from './imgLoop.styles'
