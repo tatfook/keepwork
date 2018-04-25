acfc7b296fb6e65aba94754f51072e47356fdb68
Jan <1720370638@qq.com>
2018-04-25 17:54:24 +0800

    add new styles and templates of mixLayer mod

--- src/components/adi/mod/web/mixLayer/mixLayer.md
+++ src/components/adi/mod/web/mixLayer/mixLayer.md
@@ -19,4 +19,29 @@
 
 #paragraph
 - data : //当前文档内容
+
+#button  
+- name :  //按钮名称
+- link :  //按钮点击跳转链接
+- target :  //链接从新窗口打开或本窗口打开
+
+#media2
+- src  : //当前图片地址
+- link : //当前图片跳转链接
+
+#title2
+- name : //当前标题内容
+- link : //当前标题跳转链接
+
+#subtitle2
+- name : //当前副标题内容
+- link : //当前副标题跳转链接
+
+#paragraph2
+- data : //当前文档内容
+
+#button2  
+- name :  //按钮名称
+- link :  //按钮点击跳转链接
+- target :  //链接从新窗口打开或本窗口打开
 ```
--- src/components/adi/mod/web/mixLayer/mixLayer.templates.js
+++ src/components/adi/mod/web/mixLayer/mixLayer.templates.js
@@ -9,5 +9,39 @@ export default [
         { colParagraph: 'paragraph' }
       ]
     }
+  ],
+
+  // template 1
+  [
+    {
+      colGroup: [
+        {
+          colGroupA: [
+            { colMedia: 'media' },
+            {
+              colGroupAA: [
+                { colTitle: 'title' },
+                { colSubtitle: 'subtitle' },
+                { colParagraph: 'paragraph' },
+                { colButton: 'button' }
+              ]
+            }
+          ]
+        },
+        {
+          colGroupB: [
+            { colMedia: 'media2' },
+            {
+              colGroupBA: [
+                { colTitle: 'title2' },
+                { colSubtitle: 'subtitle2' },
+                { colParagraph: 'paragraph2' },
+                { colButton: 'button2' }
+              ]
+            }
+          ]
+        }
+      ]
+    }
   ]
 ]
