dead4eb9e90b77b878db41f8a40aaad7302a0214
ZhangKaitlyn <1873070425@qq.com>
2018-04-26 18:07:28 +0800

    templates

---
+++ src/components/adi/layout/templates/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-container class="header-template">
+    <el-header height='auto'>
+      <slot name="header"></slot>
+    </el-header>
+    <el-main>
+      <slot> </slot>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.header-template{
+  .el-header, .el-main{
+    padding: 0;
+  }
+}
+</style>
--- src/components/adi/layout/templates/index.js
+++ src/components/adi/layout/templates/index.js
@@ -1,10 +1,18 @@
-import Template1 from './Template1'
-import Template2 from './Template2'
+// import Template1 from './Template1'
+// import Template2 from './Template2'
 import Template3 from './Template3'
+import Basic from './Basic'
+import BasicFullscreen from './BasicFullscreen'
+import Header from './Header'
+import HeaderSidebar from './HeaderSidebar'
 
 export default {
-  basic: Template1,
-  basic_fullscreen: Template2,
-  header: Template3,
-  header_siderbar: Template3
+  // basic: Template1,
+  basic: Basic,
+  basic_fullscreen: BasicFullscreen,
+  // basic_fullscreen: Template2,
+  // header: Template3,
+  header: Header,
+  // header_siderbar: Template3
+  header_siderbar: HeaderSidebar
 }
