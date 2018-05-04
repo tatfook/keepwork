f574a9a71142ee19a10fea82db19d7f73b0cdf31
Rango Yuan <yuanoook@foxmail.com>
2018-05-04 18:07:29 +0800

    fix Fullscreen > FullScreen

---
+++ src/components/adi/layout/templates/HeaderFullScreen.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-container class="fullscreen-template">
+    <el-header height='auto'>
+      <slot name="header"></slot>
+    </el-header>
+    <el-main>
+      <slot> </slot>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.fullscreen-template{
+  .el-header, .el-main{
+    padding: 0;
+  }
+}
+</style>
