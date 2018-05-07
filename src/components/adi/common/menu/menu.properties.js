3b20149b48c3ec5cfdb3b46a0baadb50c5887b8b
Jan <1720370638@qq.com>
2018-05-07 08:17:05 +0800

    set footer and menu comp default value

--- src/components/adi/common/menu/menu.properties.js
+++ src/components/adi/common/menu/menu.properties.js
@@ -1,48 +1,8 @@
 export default {
   data: [
     {
-      name: '新增功能',
-      link: 'http://keepwork.com',
-      child: [
-        {
-          name: '配天经营学校',
-          link: 'http://keepwork.com'
-        },
-        {
-          name: '配天讲堂1',
-          link: 'http://keepwork.com'
-        }
-      ]
-    },
-
-    {
-      name: '应用中心与支持',
-      link: 'http://keepwork.com',
-      child: [
-        {
-          name: '账户个人资料',
-          link: 'http://keepwork.com'
-        },
-        {
-          name: '应用中心',
-          link: 'http://keepwork.com'
-        }
-      ]
-    },
-
-    {
-      name: '教育',
-      link: 'http://keepwork.com',
-      child: [
-        {
-          name: 'NPL CAD编辑',
-          link: 'http://keepwork.com'
-        },
-        {
-          name: '动画创作大赛',
-          link: 'http://keepwork.com'
-        }
-      ]
+      name: 'MENU',
+      link: 'http://keepwork.com'
     }
   ],
   target: '_self'
