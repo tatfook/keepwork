d26521e18bd69c88e23289f955f5cbe8e370dd18
Leopold Chen <placeintime.qh@gmail.com>
2018-05-11 11:23:15 +0800

    feat mocha-chai-sinon test enviorment

--- config/test.env.js
+++ config/test.env.js
@@ -3,5 +3,9 @@ const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"'
+  NODE_ENV: '"testing"',
+  KEEPWORK_API_PREFIX: '"http://stage.keepwork.com/api/wiki/models"',
+  GITLAB_API_PREFIX: '"http://git.stage.keepwork.com"',
+  ELASTICSEARCH_BASE_URL: '"http://es.keepwork.com/api/v0/"',
+  QINIU_SERVICE_BASE_URL: '"http://es.keepwork.com/api/v0/"'
 })
---
+++ test/setup.js
@@ -0,0 +1,4 @@
+// setup JSDOM
+require('jsdom-global')()
+// make expect available globally
+global.expect = require('chai').expect
