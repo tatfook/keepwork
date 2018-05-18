f1df8760f3552b968e69ef5b43f6ea9fa9578b17
Rango Yuan <yuanoook@foxmail.com>
2018-05-18 16:31:02 +0800

    fix commanHeader

--- src/components/common/CommonHeader.vue
+++ src/components/common/CommonHeader.vue
@@ -48,16 +48,13 @@
           <img class="user-profile" :src='userProfile.portrait' alt="username">
         </template>
         <el-menu-item index='1-1'>
-          <a href="/wiki/home">{{$t('common.features')}}</a>
+          <a :href='"/" + userProfile.username'>{{$t('common.myHomePage')}}</a>
         </el-menu-item>
         <el-menu-item index='1-2'>
-          <a href="/wiki/apps">{{$t('common.applicationCenter')}}</a>
+          <a href="/wiki/user_center?userCenterContentType=websiteManager">{{$t('common.websiteManagement')}}</a>
         </el-menu-item>
         <el-menu-item index='1-3'>
-          <a href='/official/help/index'>{{$t('common.help')}}</a>
-        </el-menu-item>
-        <el-menu-item index='1-6'>
-          <a href='http://iicc.keepwork.com' target="_blank">{{$t('common.iicc')}}</a>
+          <a href="/wiki/wikieditor">{{$t('common.pageEditor')}}</a>
         </el-menu-item>
       </el-submenu>
       <el-submenu index='2' class="pull-right">
@@ -65,13 +62,16 @@
           <i class="el-icon-menu"></i>
         </template>
         <el-menu-item index='2-1'>
-          <a :href='"/" + userProfile.username'>{{$t('common.myHomePage')}}</a>
+          <a href="/wiki/home">{{$t('common.features')}}</a>
         </el-menu-item>
         <el-menu-item index='2-2'>
-          <a href="/wiki/user_center?userCenterContentType=websiteManager">{{$t('common.websiteManagement')}}</a>
+          <a href="/wiki/apps">{{$t('common.applicationCenter')}}</a>
         </el-menu-item>
         <el-menu-item index='2-3'>
-          <a href="/wiki/wikieditor">{{$t('common.pageEditor')}}</a>
+          <a href='/official/help/index'>{{$t('common.help')}}</a>
+        </el-menu-item>
+        <el-menu-item index='2-6'>
+          <a href='http://iicc.keepwork.com' target="_blank">{{$t('common.iicc')}}</a>
         </el-menu-item>
       </el-submenu>
     </el-menu>
