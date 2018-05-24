77f57d5e9cb5983e760666e55a45f4521b35c7b7
Jan <1720370638@qq.com>
2018-05-24 08:22:14 +0800

    fix real-time rendering bug in mixPosition mod

--- src/components/adi/mod/web/mixPosition/mixPosition.vue
+++ src/components/adi/mod/web/mixPosition/mixPosition.vue
@@ -38,21 +38,9 @@ import baseMixin from '../../base/base.mixin'
 
 export default {
   mixins: [baseMixin],
-  methods: {
-    compWrapperOptions(name) {
-      let options = {}
-
-      options = _.merge(options, this.generateOptionsStyle(name))
-
-      if (this.modData.styleID == '3' || this.modData.styleID == '4') {
-        return _.merge(
-          {},
-          options,
-          (this.modData.media.src = require('@/../static/adi/mixPosition/stitching.png'))
-        )
-      } else {
-        return options
-      }
+  mounted() {
+    if (this.modData.styleID == '3' || this.modData.styleID == '4') {
+      this.modData.media.src = require('@/../static/adi/mixPosition/stitching.png')
     }
   }
 }
