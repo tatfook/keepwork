106a0d8521017a160b81697cd497a6edc10be311
Jan <1720370638@qq.com>
2018-06-05 17:28:55 +0800

    button mod and i18n updated

--- src/components/adi/mod/web/button/index.js
+++ src/components/adi/mod/web/button/index.js
@@ -1,3 +1,27 @@
+/*doc
+---
+title: Button Mod
+name: Button Mod
+category: Adi Mod
+---
+
+This is the Button Mod for usage.
+
+```@Button
+styleID: // 样式ID
+button:
+  name: //按钮名称
+  link: // 图片点击跳转链接
+  width: // 图片宽度
+  height: // 图片高度
+  src: // 图片来源地址链接
+  fontSize: //文字字体大小
+  target: // 链接从新窗口打开或本窗口打开
+
+```
+
+*/
+
 import { generateProperties } from '@/components/adi/mod/base/base.utils'
 import mod from '@/components/adi/mod/base/Base'
 import styles from './button.styles'
