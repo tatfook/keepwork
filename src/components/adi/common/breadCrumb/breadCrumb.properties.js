003d66c63eb6d6bd4306894ab8ccd1e92ab4b5cb
Jan <1720370638@qq.com>
2018-06-19 18:07:45 +0800

    fix bug #940

--- src/components/adi/common/breadCrumb/breadCrumb.properties.js
+++ src/components/adi/common/breadCrumb/breadCrumb.properties.js
@@ -1,27 +1,7 @@
 export default {
   data: [
     {
-      name: '官网首页',
-      link: ''
-    },
-    {
-      name: '下载中心',
-      link: ''
-    },
-    {
-      name: '新闻资讯',
-      link: ''
-    },
-    {
-      name: '项目介绍',
-      link: ''
-    },
-    {
-      name: '作品交流',
-      link: ''
-    },
-    {
-      name: '游戏百科',
+      name: '',
       link: ''
     }
   ],
