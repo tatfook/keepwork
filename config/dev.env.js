06f8baa045db1a34eb51b3f1635932fd20a395a7
Leopold Chen <placeintime.qh@gmail.com>
2018-06-19 16:56:56 +0800

    fix gitlab raw url issue; rollback to git.keepwork.com

--- config/dev.env.js
+++ config/dev.env.js
@@ -7,6 +7,8 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   HOST_ENV: '"localhost"',
   KEEPWORK_API_PREFIX: `"https://stage${i18nDomainLabel('-')}.keepwork.com/api/wiki/models"`,
-  GITLAB_API_PREFIX: `"https://api-stage${i18nDomainLabel('-')}.keepwork.com/git"`,
-  ES_GATEWAY_BASE_URL: `"https://api-stage${i18nDomainLabel('-')}.keepwork.com/es"`
+  GITLAB_API_PREFIX: `"https://git-stage${i18nDomainLabel('-')}.keepwork.com"`,
+  ES_GATEWAY_BASE_URL: `"https://api-stage${i18nDomainLabel('-')}.keepwork.com/es"`,
+  ES_INDEX: `"www_pages_v1"`,
+  ES_TYPE: `"pages"`
 })
--- config/release.env.js
+++ config/release.env.js
@@ -6,6 +6,6 @@ const i18nDomainLabel = require('./i18nDomainLabel')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   KEEPWORK_API_PREFIX: `"https://release${i18nDomainLabel('-')}.keepwork.com/api/wiki/models"`,
-  GITLAB_API_PREFIX: `"https://api-release${i18nDomainLabel('-')}.keepwork.com/git"`,
+  GITLAB_API_PREFIX: `"https://git-release${i18nDomainLabel('-')}.keepwork.com"`,
   ES_GATEWAY_BASE_URL: `"https://api-release${i18nDomainLabel('-')}.keepwork.com/es"`
 })
