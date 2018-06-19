e900504b366d83deffbe54974a868d1b43b2fda9
Leopold Chen <placeintime.qh@gmail.com>
2018-06-19 16:56:56 +0800

    fix gitlab raw url issue, rollback gitr base url to git.keepwork.com

--- config/prod.env.js
+++ config/prod.env.js
@@ -5,7 +5,7 @@ module.exports = {
   BUILD_VERSION: `"${new Date().toString().split(' ').splice(0, 6).join('_').replace(/:|\+/g,'_')}"`,
   NODE_ENV: '"production"',
   KEEPWORK_API_PREFIX: `"https://${i18nDomainLabel('', '.')}keepwork.com/api/wiki/models"`,
-  GITLAB_API_PREFIX: `"https://api${i18nDomainLabel('-')}.keepwork.com/git"`,
+  GITLAB_API_PREFIX: `"https://git${i18nDomainLabel('-')}.keepwork.com"`,
   ES_GATEWAY_BASE_URL: `"https://api${i18nDomainLabel('-')}.keepwork.com/es"`,
   ES_INDEX: `""`,
   ES_TYPE: `""`
