12b5b12a0e063c4ef1ea918387e463d5ce074287
Leopold Chen <placeintime.qh@gmail.com>
2018-06-22 10:41:48 +0800

    fix stage lessons api config

--- config/stage.env.js
+++ config/stage.env.js
@@ -6,7 +6,7 @@ const i18nDomainLabel = require('./i18nDomainLabel')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   KEEPWORK_API_PREFIX: `"https://stage${i18nDomainLabel('-')}.keepwork.com/api/wiki/models"`,
-  LESSON_API_PREFIX: `"http://lesson.keepwork.com/"`,
+  LESSON_API_PREFIX: `"https://lessons-stage.keepwork.com/lessons"`,
   GITLAB_API_PREFIX: `"https://git-stage${i18nDomainLabel('-')}.keepwork.com"`,
   ES_GATEWAY_BASE_URL: `"https://api-stage${i18nDomainLabel('-')}.keepwork.com/es"`
 })
