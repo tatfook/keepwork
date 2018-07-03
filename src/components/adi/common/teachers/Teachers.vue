c841b15ec5096fc14d636b2e524b1be29a07f949
kevinxft <kevinxft@gmail.com>
2018-07-03 11:40:33 +0800

    fix i18 bug

--- src/components/adi/common/teachers/Teachers.vue
+++ src/components/adi/common/teachers/Teachers.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="comp-teachers" v-if="teacherShow">
-    <span>Teachers:</span>
+    <span>{{this.$t('card.teachersColon')}}</span>
     <pre class="content">{{properties.content ? properties.content : $t(options.content)}}</pre>
   </div>
 </template>
