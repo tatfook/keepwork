7ecf4577cb1908fcee39c8e4f148db17bd92be9c
张大伟 <favorofife@yeah.net>
2018-07-05 18:05:04 +0800

    Update release.env.js

--- config/release.env.js
+++ config/release.env.js
@@ -7,7 +7,7 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   KEEPWORK_API_PREFIX: `"https://release${i18nDomainLabel('-')}.keepwork.com/api/wiki/models"`,
   GITLAB_API_PREFIX: `"https://git-release${i18nDomainLabel('-')}.keepwork.com"`,
-  ES_GATEWAY_BASE_URL: `"https://api-stage${i18nDomainLabel('-')}.keepwork.com/es"`,
+  ES_GATEWAY_BASE_URL: `"https://api-release${i18nDomainLabel('-')}.keepwork.com/es"`,
   ES_INDEX: `"test_kw_pages"`,
   ES_TYPE: `"pages"`
 })
