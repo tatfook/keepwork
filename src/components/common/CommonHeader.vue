1332717b24c664735722cf0f0ec8a77ea2ba486b
DDXY1230 <2502752288@qq.com>
2018-07-18 11:04:32 +0800

    code optimize

--- src/components/common/CommonHeader.vue
+++ src/components/common/CommonHeader.vue
@@ -112,37 +112,8 @@
     <div @click.stop v-if='isSkyDriveManagerDialogShow'>
       <SkyDriveManagerDialog :show='isSkyDriveManagerDialogShow' @close='closeSkyDriveManagerDialog' />
     </div>
-    <div @click.stop v-if="isLoginDialogShow" class="login-dialog">
-      <el-dialog title="" :visible.sync="isLoginDialogShow">
-        <el-form class="login-dialog-form" :model="ruleForm" :rules="rules" ref="ruleForm">
-          <el-form-item prop="username">
-            <el-input v-model="ruleForm.username"></el-input>
-          </el-form-item>
-           <el-form-item prop="password">
-            <el-input type="password" v-model="ruleForm.password"></el-input>
-          </el-form-item>
-          <div class="login-dialog-form-operate"><a href="/wiki/find_pwd">{{$t('common.forgetPassword')}}?</a></div>
-          <el-form-item>
-            <el-button class="login-btn" type="primary" @click="login('ruleForm')">{{$t('common.login')}}</el-button>
-          </el-form-item>
-          <div class="login-dialog-form-operate_signIn">{{$t('common.noAccount')}}?<a href="/wiki/join">{{$t('common.signIn')}}</a></div>
-          <div class="login-dialog-form-three-login">
-            <div class="title">{{$t('common.usingFollowingAccount')}}</div>
-            <a>
-              <img src="https://stage.keepwork.com/wiki/assets/imgs/icon/wiki_qq.png" alt="">
-            </a>
-            <a>
-              <img src="https://stage.keepwork.com/wiki/assets/imgs/icon/wiki_wechat.png" alt="">
-            </a>
-            <a>
-              <img src="https://stage.keepwork.com/wiki/assets/imgs/icon/wiki_sina_weibo.png" alt="">
-            </a>
-            <a>
-              <img src="https://stage.keepwork.com/wiki/assets/imgs/icon/wiki_github_logo.png" alt="">
-            </a>
-          </div>
-        </el-form>
-      </el-dialog>
+    <div @click.stop v-if="isLoginDialogShow">
+      <LoginDialog :show="isLoginDialogShow" @close="closeLoginDialog"/>
     </div>
   </div>
 </template>
@@ -152,6 +123,7 @@ import 'element-ui/lib/theme-chalk/display.css'
 import { mapGetters, mapActions } from 'vuex'
 import PersonalCenterDialog from '@/components/common/PersonalCenterDialog'
 import SkyDriveManagerDialog from '@/components/common/SkyDriveManagerDialog'
+import LoginDialog from '@/components/common/LoginDialog'
 import Cookies from 'js-cookie'
 const IS_GLOBAL_VERSION = !!process.env.IS_GLOBAL_VERSION
 
@@ -163,16 +135,6 @@ export default {
       isPersonalCenterShow: false,
       isSkyDriveManagerDialogShow: false,
       isLoginDialogShow: false,
-      ruleForm: {
-        username: '',
-        password: ''
-      },
-      rules: {
-        username: [
-          { required: true, message: this.$t('common.inputUsername'), trigger: 'blur' }
-        ],
-        password: [{ required: true, message: this.$t('common.inputPassword'), trigger: 'blur' }]
-      }
     }
   },
   computed: {
@@ -201,7 +163,6 @@ export default {
   methods: {
     ...mapActions({
       userGetProfile: 'user/getProfile',
-      userLogin: 'user/login',
       userLogout: 'user/logout'
     }),
     backEditArea() {
@@ -231,33 +192,8 @@ export default {
     goLogin() {
       this.isLoginDialogShow = true
     },
-    async login(formName) {
-      this.$refs[formName].validate(async valid => {
-        if (valid) {
-          let payload = {
-            username: this.ruleForm.username,
-            password: this.ruleForm.password
-          }
-          let info = await this.userLogin(payload)
-          if (info.error.id === 0) {
-            this.isLoginDialogShow = false
-          } else if (info.error.message === '用户不存在') {
-            this.$message({
-              message: this.$t('common.usernameNotExist'),
-              type:'error',
-              showClose: true
-            })
-          }else if (info.error.message === '密码错误'){
-            this.$message({
-              message:  this.$t('common.wrongPassword'),
-              type:'error',
-              showClose: true
-            })
-          }
-        } else {
-          return false
-        }
-      })
+    closeLoginDialog(){
+      this.isLoginDialogShow = false
     },
     logout() {
       this.userLogout()
@@ -269,7 +205,8 @@ export default {
   },
   components: {
     PersonalCenterDialog,
-    SkyDriveManagerDialog
+    SkyDriveManagerDialog,
+    LoginDialog
   }
 }
 </script>
@@ -365,88 +302,6 @@ export default {
   width: 30px;
   height: 30px;
   margin-right: 5px;
-}
-.login-dialog {
-  .el-dialog__header {
-    padding: 0;
-  }
-  .el-dialog {
-    width: 478px;
-    height: 580px;
-    padding: 40px 0 60px 0;
-  }
-  &-form {
-    width: 68%;
-    margin: 0 auto;
-    .el-form-item__content {
-      .el-input__inner {
-        &:focus {
-          box-shadow: 1px 1px 3px #daeaf6, -1px -1px 3px #daeaf6;
-        }
-      }
-    }
-    &-operate {
-      text-align: right;
-      cursor: pointer;
-      a{
-        text-decoration: none;
-        color: inherit;
-      }
-    }
-    &-operate_signIn {
-      text-align: right;
-      a {
-        display: inline-block;
-        text-decoration: none;
-        height: 20px;
-        border-bottom: 1px solid #3977ad;
-        color: #286090;
-        cursor: pointer;
-      }
-    }
-    &-three-login {
-      a {
-        display: inline-block;
-        width: 24%;
-        text-align: center;
-        img {
-          cursor: pointer;
-        }
-      }
-      .title {
-        margin: 40px 0;
-        padding: 20px 0 35px;
-        text-align: center;
-        position: relative;
-        &::before {
-          content: '';
-          height: 2px;
-          width: 16%;
-          position: absolute;
-          right: 0;
-          top: 40%;
-          background-color: #d6e6f4;
-        }
-        &::after {
-          content: '';
-          height: 2px;
-          width: 16%;
-          position: absolute;
-          left: 0;
-          top: 40%;
-          background-color: #d6e6f4;
-        }
-      }
-    }
-    .login-btn {
-      width: 100%;
-      margin: 20px 0;
-      height: 44px;
-      padding: 10px 16px;
-      font-size: 18px;
-      border-radius: 6px;
-    }
-  }
 }
 @media (max-width: 768px) {
   .el-submenu__title {
