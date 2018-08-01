a0e1de73639040b7b69fd2b0c9ad67c59f6db750
ZhangKaitlyn <1873070425@qq.com>
2018-08-01 18:53:41 +0800

    课程包详情页区分学生和老师入口

---
+++ src/components/lesson/teacher/PackageDetailPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <PackageDetail :actorType='actorType'></PackageDetail>
+</template>
+<script>
+import PackageDetail from '../common/PackageDetail'
+export default {
+  name: 'PackageDetailPage',
+  data() {
+    return {
+      actorType: 'teacher'
+    }
+  },
+  components: {
+    PackageDetail
+  }
+}
+</script>
