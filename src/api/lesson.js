869f4dedcb904daf2b23e32fb931c44decc03568
ZhangKaitlyn <1873070425@qq.com>
2018-08-17 11:17:47 +0800

    fix login get user detail

--- src/api/lesson.js
+++ src/api/lesson.js
@@ -66,7 +66,7 @@ export const lessons = {
 }
 
 export const users = {
-  getUserDetail: args => get('users'),
+  getUserDetail: (...args) => get('users', ...args),
   userSubscribes: args => get(`users/${args.id}/subscribes`),
   userSkills: args => get(`users/${args.id}/skills`)
 }
