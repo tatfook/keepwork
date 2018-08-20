598117ff48778bd066ff13b15e08a45d45babbbf
Jan <1720370638@qq.com>
2018-08-20 18:45:04 +0800

    showComponent test

--- src/components/adi/common/ppt/ShowComponent.vue
+++ src/components/adi/common/ppt/ShowComponent.vue
@@ -1,7 +1,7 @@
 <template>
-  <el-dialog class="dialog" :title="$t('editor.pptPresentation')" :visible.sync="isDialogShow" :before-close="handleClose" center>
-    <div class="reveal">
-      <div class="slides">
+  <el-dialog class="dialog" :title="$t('editor.pptPresentation')" :visible.sync="isDialogShow" :before-close="handleClose" center>0
+    <div class="reveal">1
+      <div class="slides">2
         <!-- <section>Slide 1</section>
         <section>			
           <section>Vertical Slide 2</section>
@@ -10,22 +10,22 @@
         </section>
         <section>Slide 3</section> -->
 
-        <section data-markdown>
+        <section data-markdown>3
           <textarea data-template>
             ## Page title
             A paragraph with some text and a [link](http://hakim.se).
           </textarea>
         </section>
 
-        <section data-markdown>
-            <script type="text/template">
-                ## Page title2
-                A paragraph with some text and a [link](http://hakim.se).
-            </script>
+        <section data-markdown>4
+          <script type="text/template">
+            ## Page title2
+            A paragraph with some text and a [link](http://hakim.se).
+          </script>
         </section>
 
         <!-- <section data-markdown data-separator="---" data-separator-vertical="--"> -->
-        <section data-markdown data-separator="---">
+        <section data-markdown data-separator="---">5
           <script type="text/template">
             # 主題1
             - 主題1-內容1
@@ -42,6 +42,7 @@
             <h2>2222222</h2>
             <h3>3333333</h3>
             <hr>
+            ---
           </script>
         </section>
       </div>
@@ -56,15 +57,21 @@
 // import '/node_modules/reveal.js/css/reveal.css'
 // import '/node_modules/reveal.js/css/theme/black.css'
 
-import Reveal from 'reveal.js/js/reveal'
-import 'reveal.js/lib/js/head.min.js'
-
-// import Reveal from 'reveal.js'
+import reveal from 'reveal.js/js/reveal.js'
+// import 'reveal.js/lib/js/head.min.js'
+// import reveal from 'reveal.js'
+window.Reveal = reveal
 import Marked from 'reveal.js/plugin/markdown/marked.js'
-window.Reveal = Reveal
 window.marked = Marked
 // import Markdown  from 'reveal.js/plugin/markdown/markdown.js'
 // Markdown.initialize()
+
+import classList from 'reveal.js/lib/js/classList.js'
+// import highlight from 'reveal.js/plugin/highlight/highlight.js'
+// import search from 'reveal.js/plugin/search/search.js'
+// import zoom from 'reveal.js/plugin/zoom-js/zoom.js'
+// import notes from 'reveal.js/plugin/notes/notes.js'
+// import math from 'reveal.js/plugin/math/math.js'
 
 export default {
   // name: '',
@@ -88,12 +95,24 @@ export default {
   },
   mounted() {
     Reveal.initialize({
-      // dependencies: [
-      //   { src: 'plugin/markdown/marked.js' },
-      //   { src: 'plugin/markdown/markdown.js' },
-      //   { src: 'plugin/notes/notes.js', async: true },
-      //   { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
-      // ]
+      controls: true,
+      progress: true,
+      history: true,
+      center: true,
+
+      transition: 'slide', // none/fade/slide/convex/concave/zoom
+
+      // More info https://github.com/hakimel/reveal.js#dependencies
+      dependencies: [
+        { src: classList, condition: function() { return !document.body.classList; } },
+        { src: Marked, condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        // { src: markdown, condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        // { src: highlight, async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+        // { src: search, async: true },
+        // { src: zoom, async: true },
+        // { src: notes, async: true },
+        // { src: math, async: true }
+      ]
     })
   },
   created() {
