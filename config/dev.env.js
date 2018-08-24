ab1bb8a2fd981a0daa97dfc0df502f1d35df4cef
Leopold Chen <placeintime.qh@gmail.com>
2018-08-24 13:42:52 +0800

    fix prod config

--- config/dev.env.js
+++ config/dev.env.js
@@ -12,7 +12,7 @@ module.exports = merge(prodEnv, {
   GATEWAY_BASE_URL: `"https://api-stage${i18nDomainLabel('-')}.keepwork.com"`,
   ES_GATEWAY_BASE_URL: `"https://api-stage${i18nDomainLabel('-')}.keepwork.com/es"`,
   STORAGE_GATEWAY_BASE_URL: `"https://api-stage${i18nDomainLabel('-')}.keepwork.com/storage/v0"`,
-  LESSON_API_MOCK_PREFIX: `"http://easymock-node.keepwork.com/mock/5b596b61c0a3ad0022b14b12"`,
+  LESSON_API_MOCK_PREFIX: `"https://easymock-node.keepwork.com/mock/5b596b61c0a3ad0022b14b12"`,
   LESSON_API_PREFIX: `"https://api-stage.keepwork.com/lesson/v0"`,
   ES_INDEX: `"www_pages_v1"`,
   ES_TYPE: `"pages"`,
