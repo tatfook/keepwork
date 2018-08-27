542bca20b31b076118d6980240f2f920ade8a18b
ZhangKaitlyn <1873070425@qq.com>
2018-08-27 14:58:40 +0800

    new package U

--- src/components/lesson/teacher/CoverMediaSetter.vue
+++ src/components/lesson/teacher/CoverMediaSetter.vue
@@ -12,7 +12,12 @@
     </div>
     <div class="cover-media-setter-from-url" v-show="imageSourceType === 'url'">
       <el-input placeholder="请输入内容" v-model="urlTypeUrl">
-        <template slot="append">预览</template>
+        <template slot="append">
+          <el-popover placement="top" width="250" trigger="hover" popper-class='cover-media-setter-image-preview'>
+            <img src="http://g.hiphotos.baidu.com/image/h%3D300/sign=a102682e4fa7d933a0a8e2739d4ad194/6f061d950a7b0208d5456ddb6fd9f2d3572cc878.jpg" alt="图片预览">
+            <el-button slot="reference">预览</el-button>
+          </el-popover>
+        </template>
       </el-input>
     </div>
   </div>
@@ -56,6 +61,24 @@ export default {
     .el-input-group {
       width: 565px;
       margin-top: 18px;
+      .el-input__inner {
+        border-radius: 4px;
+      }
+      .el-input-group__append {
+        color: #333;
+        background-color: transparent;
+        border: none;
+      }
+    }
+  }
+  &-image-preview {
+    padding: 0;
+    height: 165px;
+    border-radius: 0;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
     }
   }
 }
