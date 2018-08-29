b2cb76fa86981fa14b274f44757c5b46b0f14b69
Leopold Chen <placeintime.qh@gmail.com>
2018-08-29 17:50:09 +0800

    fix issue 1711

--- src/store/editor/getters.js
+++ src/store/editor/getters.js
@@ -86,7 +86,7 @@ const getters = {
     activeAreaData && UndoHelper.canRedo(activeAreaData.undoManager),
   filemanagerTreeNodeExpandMapByPath: state =>
     state.filemanagerTreeNodeExpandMapByPath,
-  cursorPos: state => state.activePage && (state.activePage.cursorPos || { line: 0, ch: 0 }),
+  cursorPos: state => state.activePage && (state.activePage.cursorPos),
   allSiteSettings: state => state.siteSettings,
   sitePath: state => getFileSitePathByPath(state.activePageUrl),
   siteSetting: (state, { allSiteSettings, sitePath }) =>
