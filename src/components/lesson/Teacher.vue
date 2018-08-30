51d11ff4b3baed736890c55fc20b37a02996e8ca
kevinxft <kevinxft@gmail.com>
2018-08-30 11:37:30 +0800

    add login form

--- src/components/lesson/Teacher.vue
+++ src/components/lesson/Teacher.vue
@@ -1,13 +1,40 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <router-view></router-view>
+    <div @click.stop v-if="isLoginDialogShow">
+      <login-dialog :show="isLoginDialogShow" @close="closeLoginDialog"></login-dialog>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import LoginDialog from '@/components/common/LoginDialog'
 export default {
-
+  data() {
+    return {
+      isLoginDialogShow: false
+    }
+  },
+  components: {
+    LoginDialog
+  },
+  methods: {
+    closeLoginDialog() {
+      this.isLoginDialogShow = false
+    }
+  },
+  beforeRouteUpdate({ name }, from, next) {
+    if (name === 'TeacherColumn' && !this.isLogined) {
+      this.isLoginDialogShow = true
+      return next(false)
+    }
+    next()
+  },
+  computed: {
+    ...mapGetters({
+      isLogined: 'user/isLogined'
+    })
+  }
 }
 </script>
-
-<style lang="sass">
-
-</style>
