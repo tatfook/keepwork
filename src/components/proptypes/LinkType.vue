fa7909d2205faef32bc32be6d35a3ce463a7293b
Jan <1720370638@qq.com>
2018-09-04 18:24:18 +0800

    fix issue 1805

--- src/components/proptypes/LinkType.vue
+++ src/components/proptypes/LinkType.vue
@@ -1,17 +1,40 @@
 <template>
-  <el-input class="link-type" :placeholder='$t("field."+editingKey)' v-model='inputTypeValue' clearable @change='updateValue' @focus='getFocus'></el-input>
+  <el-input :placeholder="$t('editor.pleaseInput')" v-model="linkTypeValue" @change='updateValue' class="input-with-select">
+    <el-button v-if="linkTypeValue" slot="prepend" icon="iconfont icon-link_"></el-button>
+    <el-button v-else slot="prepend">{{$t('common.link')}}</el-button>
+    <el-select v-model="linkTypeValue" slot="append" placeholder="Select">
+      <el-option v-for="(path, pathIndex) in personalAllPagePathList" :key="pathIndex" :value="locationOrigin + '/' + path">
+        {{ path }}
+      </el-option>
+    </el-select>
+  </el-input>
 </template>
+
 <script>
 import protypesBaseMixin from './protypes.base.mixin'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'LinkType',
   mixins: [protypesBaseMixin],
   props: {
+    editingKey: String,
     originValue: String
   },
+  data() {
+    return {
+      linkTypeValue: '',
+      locationOrigin: location.origin
+    }
+  },
+  async mounted() {
+    await this.getAllPersonalPageList()
+  },
   computed: {
-    inputTypeValue: {
+    ...mapGetters({
+      personalAllPagePathList: 'user/personalAllPagePathList'
+    }),
+    linkTypeValue: {
       get() {
         return this.originValue
       },
@@ -19,26 +42,39 @@ export default {
     }
   },
   methods: {
+    ...mapActions({
+      getAllPersonalPageList: 'user/getAllPersonalPageList'
+    }),
     updateValue(newVal) {
       var tempChangedDataObj = {}
       tempChangedDataObj[this.editingKey] = newVal
       this.$emit('onPropertyChange', tempChangedDataObj)
-    },
-    getFocus() {
-      this.$emit('onChangeValue')
     }
   }
 }
 </script>
-<style>
-.link-type .el-input__inner {
-  border: none;
-  border-bottom: 1px solid #e4e7ed;
-  font-size: 16px;
-  padding: 18px 0 0;
-  border-radius: 0;
+
+<style lang="scss">
+.el-input-group__prepend {
+  padding: 0;
+  .el-button--default {
+    padding: 0;
+    margin: 0;
+  }
 }
-.link-type .el-input__suffix {
-  top: 10px;
+.input-with-select {
+  border-bottom: 1px solid #e4e7ed;
+  > .el-input-group__prepend,
+  > .el-input__inner,
+  > .el-input-group__append {
+    border: none;
+    background: none;
+  }
+  > .el-input__inner {
+    padding: 0 0 0 5px;
+  }
+  > .el-input-group__append {
+    padding-left: 12px;
+  }
 }
 </style>
