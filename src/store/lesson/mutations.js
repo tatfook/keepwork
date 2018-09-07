542848edee953f3f37a36836cabb393b51c16930
kevinxft <kevinxft@gmail.com>
2018-09-07 18:46:21 +0800

    fix logout bug

--- src/store/lesson/mutations.js
+++ src/store/lesson/mutations.js
@@ -5,18 +5,23 @@ const GET_PACKAGE_DETAIL_SUCCESS = 'GET_PACKAGE_DETAIL_SUCCESS'
 const GET_LESSON_DETAIL_SUCCESS = 'GET_LESSON_DETAIL_SUCCESS'
 const GET_ALL_SKILLS_SUCCESS = 'GET_ALL_SKILLS_SUCCESS'
 const GET_ALL_SUBJECTS_SUCCESS = 'GET_ALL_SUBJECTS_SUCCESS'
+const LOGOUT = 'LOGOUT'
 
 export const props = {
   GET_USER_INFO_SUCCESS,
   GET_PACKAGE_DETAIL_SUCCESS,
   GET_LESSON_DETAIL_SUCCESS,
   GET_ALL_SKILLS_SUCCESS,
-  GET_ALL_SUBJECTS_SUCCESS
+  GET_ALL_SUBJECTS_SUCCESS,
+  LOGOUT
 }
 
 const mutations = {
   [GET_USER_INFO_SUCCESS](state, userDetailInfo) {
     Vue.set(state, 'userinfo', userDetailInfo)
+  },
+  [LOGOUT](state) {
+    Vue.set(state, 'userinfo', {})
   },
   [GET_PACKAGE_DETAIL_SUCCESS](state, { detail }) {
     Vue.set(state, 'packagesDetail', {
