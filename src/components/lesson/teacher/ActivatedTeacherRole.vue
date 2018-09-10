70e0ded30191599c8cec7b05059039f9457b3f57
ZhangKaitlyn <1873070425@qq.com>
2018-09-10 11:06:39 +0800

    教师专栏菜单样式

--- src/components/lesson/teacher/ActivatedTeacherRole.vue
+++ src/components/lesson/teacher/ActivatedTeacherRole.vue
@@ -2,7 +2,7 @@
   <div class="activated-teacher-role">
     <el-container class="teacher">
       <el-aside width="260px">
-        <el-menu :default-active="itmeActive" class="el-menu-vertical-demo">
+        <el-menu :default-active="itmeActive" class="el-menu-vertical-demo" text-color='#b3b3b3' active-text-color='#fff'>
           <el-menu-item index="1" @click="showItem('TEACH')">
             <i class="iconfont icon-teach"></i>
             <span class="item-title" slot="title">{{$t('lesson.teach')}}</span>
@@ -112,34 +112,50 @@ export default {
     height: 100%;
     .el-aside {
       padding: 80px 0;
-      background: #fff;
-      border-right: 10px solid #409efe;
+      background: #2f3541;
+      border-right: 8px solid #409efe;
       overflow: hidden;
       margin-right: 38px;
       .el-menu {
         border: none;
-        .el-menu-item {
-          border-bottom: 1px solid #d2d2d2;
-        }
-        .is-active {
-          background: #409efe;
-          color: #fff !important;
-          .item-title {
-            color: white;
+        background-color: transparent;
+        .el-menu-item,
+        .el-submenu__title {
+          height: 60px;
+          line-height: 60px;
+          padding: 0 15px 0 10px;
+          padding-left: 10px !important;
+          font-weight: bold;
+          i {
+            font-size: 24px;
+            margin-right: 10px;
+            font-weight: normal;
           }
-          .iconfont {
-            color: white !important;
+          .el-icon-arrow-down {
+            font-size: 16px;
+            margin-right: unset;
+            right: 10px;
           }
         }
-        .item-title {
-          font-weight: 700;
-          color: #111;
-          font-size: 14px;
-          height: 63px;
-          line-height: 63px;
-          cursor: pointer;
-          padding: 0 10px;
-          margin-left: 12px;
+        .el-menu-item.is-active,
+        .el-menu-item.is-active:hover {
+          background-color: #409efe;
+        }
+        .el-menu-item:hover,
+        .el-submenu__title:hover {
+          background-color: #434a50;
+        }
+        .el-submenu .el-menu-item {
+          height: 40px;
+          line-height: 40px;
+          padding-left: 50px !important;
+          font-weight: normal;
+        }
+        .el-submenu.is-active .el-submenu__title {
+          .item-title,
+          i {
+            color: #fff;
+          }
         }
       }
     }
