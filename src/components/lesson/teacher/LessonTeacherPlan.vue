25a96b72599dbdbfa71c91f10d046b3efab3eed4
kevinxft <kevinxft@gmail.com>
2018-09-13 14:10:43 +0800

    fix lesson mod bug

--- src/components/lesson/teacher/LessonTeacherPlan.vue
+++ src/components/lesson/teacher/LessonTeacherPlan.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="lesson-plan">
     <lesson-hint-toggle />
-    <lesson-wrap v-for="(item,index) in lessonMain" :key="index" :data="item" :isPreview="true" :isTeacher="true" />
+    <lesson-wrap v-for="mod in lessonMain" :mod="mod" :key="mod.key" :isPreview="true" :isTeacher="true" />
   </div>
 </template>
 
