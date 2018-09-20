e4496a91e701b143cefa65f918deccaf810f23f5
Leopold Chen <placeintime@live.cn>
2018-09-20 19:46:39 +0800

    Revert "adjust common header"
    This reverts commit fd95273f41d2d3fc5ef9314e0319d5255542e27c.

--- src/components/common/SearchBar.vue
+++ src/components/common/SearchBar.vue
@@ -1,9 +1,9 @@
 <template>
   <el-input v-model="searchText" class="search-bar-comp" @keyup.enter.native='goSearch'>
-    <!-- <el-select class="search-bar-comp-select" v-model="searchScope" slot="prepend">
+    <el-select class="search-bar-comp-select" v-model="searchScope" slot="prepend">
       <el-option :label="$t('search.searchAll')" value="all"></el-option>
       <el-option :label="$t('search.searchMine')" value="loginUser" v-show="isLogin"></el-option>
-    </el-select> -->
+    </el-select>
     <el-button slot="append" icon="el-icon-search" @click="goSearch"></el-button>
   </el-input>
 </template>
@@ -48,8 +48,7 @@ export default {
 .search-bar-comp {
   .el-input__inner {
     border-right: none;
-    width: 240px;
-    height: 32px;
+    width: 100px;
     padding: 0 0 0 10px;
   }
   .el-input__inner:hover,
@@ -71,7 +70,7 @@ export default {
   }
   .el-input-group__append .el-button {
     margin: 0;
-    padding: 7px 4px;
+    padding: 9px 4px;
   }
   .el-icon-search {
     position: relative;
