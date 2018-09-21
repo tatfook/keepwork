3f0c19c46e88e5b29bd9d716d74b30526a242175
Jan <1720370638@qq.com>
2018-09-21 09:24:48 +0800

    fix height of comment style

--- src/components/adi/common/comment/Comment.vue
+++ src/components/adi/common/comment/Comment.vue
@@ -260,7 +260,7 @@ export default {
           resize: none;
           box-sizing: border-box;
           width: 100%;
-          height: 50px;
+          height: 74px;
           border: 2px solid #d4d4d4;
           outline: none;
           background-color: #eeeeee;
