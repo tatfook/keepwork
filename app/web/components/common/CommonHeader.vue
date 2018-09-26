d2ff1071d84b56f7e5f25b894e01ba9c6ed4f6a3
DDXY1230 <2502752288@qq.com>
2018-09-26 15:17:16 +0800

    explore tab

--- app/web/components/common/CommonHeader.vue
+++ app/web/components/common/CommonHeader.vue
@@ -5,10 +5,10 @@
         <img class="brand" src="@/assets/img/logo_old.svg" alt="KeepWork">
       </el-menu-item>
       <el-menu-item index='1'>
-        <a href="/wiki/home">{{$t('common.creativity')}}</a>
+        <a href="/#/creativity">{{$t('common.creativity')}}</a>
       </el-menu-item>
       <el-menu-item index='2'>
-        <a href="/wiki/apps">{{$t('common.explore')}}</a>
+        <a href="/#/exploration">{{$t('common.explore')}}</a>
       </el-menu-item>
       <el-menu-item index='4'>
         <a href='/official/help/index'>{{$t('common.study')}}</a>
--- app/web/components/common/ExplorationPage.vue
+++ app/web/components/common/ExplorationPage.vue
@@ -30,19 +30,73 @@
     </div>
     <div class="exploration-page-cabinet">
       <div class="exploration-page-cabinet-center">
-        <div class="options">
-          <el-button type="primary">项目</el-button>
-          <el-button>3D世界</el-button>
-          <el-button>网站</el-button>
-          <el-button>知识</el-button>
-          <el-button>课程</el-button>
-          <el-button>用户</el-button>
-          <el-button>工作室</el-button>
-          <el-button>招募中</el-button>
+        <div class="options" ref="options" @click="selectTab($event)">
+          <el-button :class="{'selected':1==currIndex}">项目</el-button>
+          <el-button :class="{'selected':2==currIndex}">3D世界</el-button>
+          <el-button :class="{'selected':3==currIndex}">网站</el-button>
+          <el-button :class="{'selected':4==currIndex}">知识</el-button>
+          <el-button :class="{'selected':5==currIndex}">课程</el-button>
+          <el-button :class="{'selected':6==currIndex}">用户</el-button>
+          <el-button :class="{'selected':7==currIndex}">工作室</el-button>
+          <el-button :class="{'selected':8==currIndex}">招募中</el-button>
         </div>
-        <div class="selected-projects">
-          <project-cell></project-cell>
+        <div class="selected-projects" v-if='currIndex == 1'>
+          <el-row>
+            <el-col :span="6">
+              <project-cell></project-cell>
+            </el-col>
+          </el-row>
         </div>
+        <div class="selected-lessons" v-if='currIndex == 5'>
+
+        </div>
+        <div class="selected-user" v-if='currIndex == 6'>
+          <ul class="selected-user-list">
+            <li class="user">
+              <div class="portrait">
+                <img src="http://star.rayli.com.cn/public/upload/share/000/001/658/04/bcfec7af1f18a45ddc90ec7f9d40a649OJWppZ.jpeg" alt="">
+              </div>
+                <div class="nickname">
+                  <h5 class="name">告诉果果</h5>
+                  <p class="desc">项目：<span class="total">1234</span> 粉丝：<span class="total">123456</span></p>
+                  <el-button type="primary" class="follow">关注</el-button>
+                </div>
+                <div class="cover">
+                  <img src="http://star.rayli.com.cn/public/upload/share/000/001/658/04/bcfec7af1f18a45ddc90ec7f9d40a649OJWppZ.jpeg" alt="">
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="selected-studio" v-if='currIndex == 7'>
+          <el-row>
+            <el-col :span="6">
+              <div class="studio">
+                <img class="studio-cover" src="http://star.rayli.com.cn/public/upload/share/000/001/658/04/bcfec7af1f18a45ddc90ec7f9d40a649OJWppZ.jpeg" alt="">
+                <h5 class="studio-name">笑傲江湖</h5>
+                <p class="studio-brief">你好，这里是笑傲江湖工作室</p>
+                <div class="studio-abstract">
+                  <div>
+                    <p class="title">项目</p>
+                    <p class="amount">123456</p>
+                  </div>
+                  <div class="member">
+                    <p class="title">成员</p>
+                    <p class="amount">123456</p>
+                  </div>
+                  <div>
+                    <p class="title">粉丝</p>
+                    <p class="amount">1235689</p>
+                  </div>
+                </div>
+                <div class="studio-jion">
+                  <el-button type="primary" class="studio-jion-button">申请加入</el-button>
+                  <el-button class="studio-jion-button">关注</el-button>
+                </div>
+              </div>
+            </el-col>
+          </el-row>
+        </div>
+        <div class="selected-recruitment" v-if='currIndex == 8'></div>
       </div>
     </div>
   </div>
@@ -54,11 +108,44 @@ export default {
   name: 'ExplorationPage',
   data() {
     return {
+      currIndex: 1
     }
   },
   methods: {
+    goSearch() {},
+    selectTab(ev) {
+      switch (ev.target.innerText) {
+        case '项目':
+          this.currIndex = 1
+          break
+        case '3D世界':
+          this.currIndex = 2
+          break
+        case '网站':
+          this.currIndex = 3
+          break
+        case '知识':
+          this.currIndex = 4
+          break
+        case '课程':
+          this.currIndex = 5
+          break
+        case '用户':
+          this.currIndex = 6
+          break
+        case '工作室':
+          this.currIndex = 7
+          break
+        case '招募中':
+          this.currIndex = 8
+          break
+        default:
+          this.currIndex = 1
+          break
+      }
+    }
   },
-  components:{
+  components: {
     ProjectCell
   }
 }
@@ -92,6 +179,7 @@ export default {
           line-height: 32px;
           font-size: 14px;
           color: #aaa;
+          cursor: pointer;
         }
       }
     }
@@ -102,14 +190,145 @@ export default {
       max-width: 1200px;
       margin: 0 auto;
       padding: 24px 0 56px;
-      .options{
+      .options {
         width: 55%;
         margin: 0 auto 24px;
-        .el-button{
+        .el-button {
           height: 24px;
           padding: 0 15px;
           font-size: 14px;
           border-radius: 12px;
+          margin-top: 4px;
+        }
+        .selected {
+          background: #409eff;
+          color: #fff;
+        }
+        .selected:hover {
+          background: rgb(24, 138, 252);
+        }
+      }
+      .selected-user {
+        &-list {
+          list-style: none;
+          .user {
+            height: 182px;
+            background: #fff;
+            display: flex;
+            border: 1px solid #e8e8e8;
+            .portrait {
+              width: 144px;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              img {
+                width: 96px;
+                height: 96px;
+                border-radius: 50%;
+                object-fit: cover;
+              }
+            }
+            .nickname {
+              flex: 1;
+              padding-top: 50px;
+              .name {
+                margin: 5px 0;
+                font-size: 16px;
+              }
+              .desc {
+                margin: 5px 0;
+                font-size: 14px;
+                color: #909399;
+                .total {
+                  color: #303133;
+                }
+              }
+              .follow {
+                height: 32px;
+                padding: 0 32px;
+                margin-top: 10px;
+              }
+            }
+            .cover {
+              width: 268px;
+              padding: 16px;
+              img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+              }
+            }
+          }
+        }
+      }
+      .selected-studio {
+        .el-row {
+          .studio {
+            width: 286px;
+            border: 1px solid #e8e8e8;
+            background: #fff;
+            padding: 30px 0;
+            text-align: center;
+            &-cover {
+              width: 96px;
+              height: 96px;
+              border-radius: 50%;
+              object-fit: cover;
+            }
+            &-name {
+              font-size: 16px;
+              color: #333;
+              margin: 18px 0;
+            }
+            &-brief {
+              font-size: 12px;
+              color: #999;
+              margin: 9px 0;
+            }
+            &-abstract {
+              display: flex;
+              div {
+                flex: 1;
+                .title {
+                  font-size: 13px;
+                  color: #666;
+                }
+                .amount {
+                  font-size: 18px;
+                  color: #333;
+                }
+              }
+              .member {
+                position: relative;
+              }
+              .member::before {
+                content: '';
+                width: 1px;
+                height: 20px;
+                background: rgb(202, 200, 200);
+                position: absolute;
+                left: 0;
+                top: 32px;
+              }
+              .member::after {
+                content: '';
+                width: 1px;
+                height: 20px;
+                background: rgb(202, 200, 200);
+                position: absolute;
+                right: 0;
+                top: 32px;
+              }
+            }
+            &-jion {
+              padding-top: 6px;
+              &-button {
+                height: 32px;
+                width: 108px;
+                padding: 0;
+              }
+            }
+          }
         }
       }
     }
