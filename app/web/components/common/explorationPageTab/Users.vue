2c1fd506836e2a426f9d9b8515c81659a82ee682
DDXY1230 <2502752288@qq.com>
2018-11-12 15:01:09 +0800

    userCell

--- app/web/components/common/explorationPageTab/Users.vue
+++ app/web/components/common/explorationPageTab/Users.vue
@@ -2,30 +2,8 @@
   <div class="user-tab">
     <div class="search-result-total">搜索到：<span>{{usersCount}}</span>个结果</div>
     <el-row>
-      <el-col :sm="12" :md="6" v-for="(user,index) in allUsersData" :key="user.id">
-        <div class="user-tab">
-          <img class="user-tab-cover" @click="goUserHomePage(user, index)" :src="user.portrait || default_portrait" alt="">
-          <h5 class="user-tab-name" @click="goUserHomePage(user, index)">{{user.username}}</h5>
-          <p class="user-tab-brief">{{user.desc || '这家伙很懒，没有简介！'}}</p>
-          <div class="user-tab-abstract">
-            <div>
-              <p class="title">项目</p>
-              <p class="amount">{{user.total_projects || 0}}</p>
-            </div>
-            <div class="member">
-              <p class="title">关注</p>
-              <p class="amount">{{user.total_follows || 0}}</p>
-            </div>
-            <div>
-              <p class="title">粉丝</p>
-              <p class="amount">{{user.total_fans || 0}}</p>
-            </div>
-          </div>
-          <div class="user-tab-jion">
-            <el-button type="primary" :class="['user-tab-jion-button',{'is-followed': isFollow(user,index)}]" :loading='isFollowLoading[index]' @click="toggleFollow(user,index)">{{isFollow(user,index) ? '已关注' : '+ 关注'}}</el-button>
-            <el-button class="user-tab-jion-button" @click="goUserHomePage(user,index)">主页</el-button>
-          </div>
-        </div>
+      <el-col :sm="12" :md="6" v-for="(user) in allUsersData" :key="user.id">
+        <user-cell :user="user"></user-cell>
       </el-col>
     </el-row>
     <div class="all-projects-pages" v-if="usersCount > perPage">
@@ -42,6 +20,7 @@ import _ from 'lodash'
 import { mapActions, mapGetters } from 'vuex'
 import default_portrait from '@/assets/img/default_portrait.png'
 import { keepwork, EsAPI } from '@/api'
+import UserCell from './UserCell'
 
 export default {
   name: 'Users',
@@ -67,6 +46,9 @@ export default {
       Array(this.allUsersData.length)
     ).map(() => false)
     this.loading = false
+  },
+  components: {
+    UserCell
   },
   computed: {
     ...mapGetters({
@@ -96,9 +78,7 @@ export default {
       getUserFavorite: 'pbl/getUserFavorite',
       toggleLoginDialog: 'pbl/toggleLoginDialog'
     }),
-    showMessage({ type = 'success', message = '操作成功' }) {
-      this.$message({ type, message })
-    },
+
     async targetPage(targetPage) {
       this.currentPage = targetPage
       this.loading = true
@@ -130,137 +110,12 @@ export default {
           this.userAllFollows = _.get(res, 'rows', [])
         })
     },
-    async toggleFollow(user, index) {
-      if (!this.isLogined) {
-        return this.toggleLoginDialog(true)
-      }
-      this.isFollowLoading[index] = true
-      if (!this.isFollow(user, index)) {
-        await keepwork.favorites
-          .favoriteProject({ objectId: user.id, objectType: 0 })
-          .then(res => {
-            this.showMessage({
-              message: '关注成功'
-            })
-            this.targetPage(this.currentPage)
-            this.getFollows()
-            this.isFollowLoading[index] = false
-          })
-          .catch(err => {
-            this.showMessage({
-              message: '关注失败'
-            })
-            this.isFollowLoading[index] = false
-          })
-      } else {
-        await keepwork.favorites
-          .unFavoriteProject({ objectId: user.id, objectType: 0 })
-          .then(res => {
-            this.showMessage({
-              message: '取消关注成功'
-            })
-            this.targetPage(this.currentPage)
-            this.getFollows()
-            this.isFollowLoading[index] = false
-          })
-          .catch(err => {
-            this.showMessage({
-              message: '取消关注失败'
-            })
-            this.isFollowLoading[index] = false
-          })
-      }
-    },
-    goUserHomePage() {
-      this.$message({
-        type: 'warning',
-        message: '程序员小姐姐努力开发中'
-      })
-    }
+    
+    
   }
 }
 </script>
 <style lang="scss">
-.user-tab {
-  .el-row {
-    .user-tab {
-      width: 286px;
-      border: 1px solid #e8e8e8;
-      background: #fff;
-      padding: 30px 0;
-      text-align: center;
-      margin: 0 auto 10px;
-      &-cover {
-        width: 96px;
-        height: 96px;
-        border-radius: 50%;
-        object-fit: cover;
-        cursor: pointer;
-      }
-      &-name {
-        font-size: 16px;
-        color: #333;
-        margin: 18px 0;
-        line-height: 22px;
-        cursor: pointer;
-      }
-      &-brief {
-        font-size: 12px;
-        color: #999;
-        margin: 9px 0;
-        line-height: 16px;
-      }
-      &-abstract {
-        display: flex;
-        div {
-          flex: 1;
-          .title {
-            font-size: 13px;
-            color: #666;
-          }
-          .amount {
-            line-height: 18px;
-            font-size: 18px;
-            color: #333;
-          }
-        }
-        .member {
-          position: relative;
-        }
-        .member::before {
-          content: '';
-          width: 1px;
-          height: 20px;
-          background: rgb(202, 200, 200);
-          position: absolute;
-          left: 0;
-          top: 32px;
-        }
-        .member::after {
-          content: '';
-          width: 1px;
-          height: 20px;
-          background: rgb(202, 200, 200);
-          position: absolute;
-          right: 0;
-          top: 32px;
-        }
-      }
-      &-jion {
-        padding-top: 6px;
-        &-button {
-          height: 32px;
-          width: 108px;
-          padding: 0;
-        }
-      }
-      .is-followed {
-        background: #5fe1af;
-        border: 1px solid #5fe1af;
-      }
-    }
-  }
-}
 </style>
 
 
