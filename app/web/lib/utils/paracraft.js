46f10aafa5000640885ed6af884a0e29a0956edf
kevinxft <kevinxft@gmail.com>
2018-11-15 14:07:18 +0800

    fix paracraft button

--- app/web/lib/utils/paracraft.js
+++ app/web/lib/utils/paracraft.js
@@ -1,9 +1,36 @@
-
-const getUrl = ({ protocol = 'paracraft', paramA = '', paramB = '', usertoken = '', link = '' }) => {
+const getUrl = ({
+  protocol = 'paracraft',
+  paramA = '',
+  paramB = '',
+  usertoken = '',
+  link = ''
+}) => {
   let actualUrl = `protocol="${protocol}" paramA="${paramA}" paramB="${paramB}" usertoken="${usertoken}" cmd/loadworld ${link}`
   let encodeUrl = encodeURIComponent(actualUrl)
   return `${protocol}://${encodeUrl}`
 }
+
+const getOpenUrl = ({
+  protocol = 'paracraft',
+  paramA = '',
+  paramB = '',
+  usertoken = ''
+}) => {
+  let hostname = window.location.hostname
+  let env = null
+  if (/^release/.test(hostname)) {
+    env = 'RELEASE'
+  }
+  if (/^stage/.test(hostname)) {
+    env = 'STAGE'
+  }
+  let actualUrl = env
+    ? `protocol="${protocol}" paramA="${paramA}" paramB="${paramB}" usertoken="${usertoken}" env="${env}" `
+    : `protocol="${protocol}" paramA="${paramA}" paramB="${paramB}" usertoken="${usertoken}"`
+  let encodeUrl = encodeURIComponent(actualUrl)
+  return `${protocol}://${encodeUrl}`
+}
 export default {
-  getUrl
+  getUrl,
+  getOpenUrl
 }
