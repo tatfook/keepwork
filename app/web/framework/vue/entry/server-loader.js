79c3ad29515ec521832b6cb0da0f747bcec8facc
qinghua.chen <placeintime@live.cn>
2018-11-16 16:45:13 +0800

    fix ie issues

--- app/web/framework/vue/entry/server-loader.js
+++ app/web/framework/vue/entry/server-loader.js
@@ -2,6 +2,7 @@
 module.exports = function(source) {
   this.cacheable()
   return `
+    import 'babel-polyfill';
     import render from 'server';
     import Page from '${this.resourcePath.replace(/\\/g, '\\\\')}';
     export default render({ ...Page });
