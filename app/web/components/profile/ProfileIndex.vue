c47bcf7510be21f2c6cc6caa42e3e1bd90d5dd31
ZhangKaitlyn <1873070425@qq.com>
2018-11-22 14:23:02 +0800

    user project count info empty

--- app/web/components/profile/ProfileIndex.vue
+++ app/web/components/profile/ProfileIndex.vue
@@ -10,18 +10,24 @@
           <div class="profile-index-infos-item">
             <i class="iconfont icon-folder-open"></i>
             <span class="profile-index-infos-label">项目</span>
-            <div class="profile-index-infos-msg">
+            <div class="profile-index-infos-msg" v-if="projectCount > 0">
               <span class="profile-index-infos-msg-item">
                 <span class="profile-index-infos-count">54</span>个项目
               </span>
+              <i class="iconfont icon-comma"></i>
               <span class="profile-index-infos-msg-item">
                 <span class="profile-index-infos-count">41</span>个3D世界
               </span>
+              <i class="iconfont icon-comma"></i>
               <span class="profile-index-infos-msg-item">
                 <span class="profile-index-infos-count">13</span>个网站
               </span>
             </div>
+            <div class="profile-index-infos-intro" v-else>
+              项目是什么？如何创建项目？<span class="profile-index-infos-intro-link" @click="showLearnGuide">了解更多</span>
+            </div>
             <router-link class="profile-index-infos-link" :to='{name:"ProfileProjectPage"}'>项目主页<i class="el-icon-arrow-right"></i></router-link>
+            <create-project-guide :showGuideDialog="showGuideDialog" @close="closeLearnGuide"></create-project-guide>
           </div>
         </div>
       </div>
@@ -29,6 +35,7 @@
   </div>
 </template>
 <script>
+import CreateProjectGuide from '@/components/common/CreateProjectGuide'
 import UserBasicMsg from './common/UserBasicMsg'
 import UserCertificates from './common/UserCertificates'
 export default {
@@ -39,9 +46,24 @@ export default {
       required: true
     }
   },
+  data() {
+    return {
+      projectCount: 20,
+      showGuideDialog: false
+    }
+  },
+  methods: {
+    showLearnGuide() {
+      this.showGuideDialog = true
+    },
+    closeLearnGuide() {
+      this.showGuideDialog = false
+    }
+  },
   components: {
     UserBasicMsg,
-    UserCertificates
+    UserCertificates,
+    CreateProjectGuide
   }
 }
 </script>
@@ -70,7 +92,7 @@ export default {
       height: 70px;
       display: flex;
       align-items: center;
-      .iconfont {
+      .iconfont:not(.icon-comma) {
         display: inline-block;
         width: 42px;
         height: 42px;
@@ -80,6 +102,15 @@ export default {
         color: #2397f3;
         font-size: 24px;
         border-radius: 4px;
+      }
+      .icon-comma {
+        color: #ddd;
+        font-size: 12px;
+        vertical-align: text-bottom;
+        bottom: -5px;
+        position: relative;
+        transform: scale(0.8);
+        display: inline-block;
       }
     }
     &-label {
@@ -93,6 +124,17 @@ export default {
       &-item {
         font-size: 14px;
         color: #a0a4aa;
+      }
+    }
+    &-intro {
+      flex: 1;
+      font-size: 14px;
+      color: #c0c4cc;
+      &-link {
+        font-size: 12px;
+        cursor: pointer;
+        color: #3ea4fa;
+        margin-left: 8px;
       }
     }
     &-count {
