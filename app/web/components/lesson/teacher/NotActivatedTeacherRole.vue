3055a77e3860f0335f6d15b07b5ad6e15d0c9048
ZhangKaitlyn <1873070425@qq.com>
2018-11-30 15:17:42 +0800

    translation

--- app/web/components/lesson/teacher/NotActivatedTeacherRole.vue
+++ app/web/components/lesson/teacher/NotActivatedTeacherRole.vue
@@ -16,51 +16,7 @@
           </el-form-item>
         </el-form>
       </div>
-      <div class="teacher-acquire">
-      <h4 class="teacher-acquire-title">{{$t('lesson.whatYouWillGet')}}</h4>
-      <div class="teacher-acquire-box">
-        <div class="acquire-item" v-for="n in 2" :key="n">
-          <div :class="['role',n === 2 ? 'role-teacher':'']">
-            <div class="role-text">{{n === 1 ? $t('lesson.learners') : $t('lesson.instructors')}}</div>
-            <span class="role-cost">{{n === 1 ?  $t('lesson.free') : `￥5000${$t('lesson.perYearPerPerson')}`}}</span>
-          </div>
-          <div class="access">
-            <p>
-              <span class="img-wrap"><img src="@/assets/lessonImg/legal_privilege.png" alt=""></span>{{$t('lesson.selfLearning')}}</p>
-            <div class="teaching-function">
-              <p>
-                <span class="img-wrap"><img src="@/assets/lessonImg/legal_privilege.png" alt=""></span>{{$t('lesson.learnAmazingLessons')}}<span class="lihgt">({{n == 1 ? $t('lesson.partiallyFree'):$t('lesson.allFree')}})</span></p>
-              <p>
-                <span class="img-wrap"><img src="@/assets/lessonImg/legal_privilege.png" alt=""></span>{{$t('lesson.realTimeFeedback')}}</p>
-            </div>
-            <p :class="{'not-student-privilege-text': n === 1}">
-              <span class="img-wrap"><img :class="{'not-student-privilege': n === 1}" src="@/assets/lessonImg/legal_privilege.png" alt=""></span>{{$t('lesson.lessonDevelopment')}}</p>
-            <div class="teaching-function">
-              <p :class="{'not-student-privilege-text': n === 1}">
-                <span class="img-wrap"><img :class="{'not-student-privilege': n === 1}" src="@/assets/lessonImg/legal_privilege.png" alt=""></span>{{$t('lesson.createLesson')}}</p>
-              <p :class="{'not-student-privilege-text': n === 1}">
-                <span class="img-wrap"><img :class="{'not-student-privilege': n === 1}" src="@/assets/lessonImg/legal_privilege.png" alt=""></span>{{$t('lesson.submitLessons')}}</p>
-              <p :class="{'not-student-privilege-text': n === 1}">
-                <span class="img-wrap"><img :class="{'not-student-privilege': n === 1}" src="@/assets/lessonImg/legal_privilege.png" alt=""></span>{{$t("lesson.getBenefit")}}</p>
-            </div>
-            <p :class="{'not-student-privilege-text': n === 1}">
-              <span class="img-wrap"><img :class="{'not-student-privilege': n === 1}" src="@/assets/lessonImg/legal_privilege.png" alt=""></span>{{$t('lesson.classroomTeaching')}}</p>
-            <div class="teaching-function">
-              <p :class="{'not-student-privilege-text': n === 1}">
-                <span class="img-wrap"><img :class="{'not-student-privilege': n === 1}" src="@/assets/lessonImg/legal_privilege.png" alt=""></span>{{$t('lesson.freeForClassroomTeaching')}}</p>
-              <p :class="{'not-student-privilege-text': n === 1}">
-                <span class="img-wrap"><img :class="{'not-student-privilege': n === 1}" src="@/assets/lessonImg/legal_privilege.png" alt=""></span>{{$t('lesson.freeForClassroomLearning')}}</p>
-              <p :class="{'not-student-privilege-text': n === 1}">
-                <span class="img-wrap"><img :class="{'not-student-privilege': n === 1}" src="@/assets/lessonImg/legal_privilege.png" alt=""></span>{{$t('lesson.trackStudentsPerformance')}}</p>
-              <p :class="{'not-student-privilege-text': n === 1}">
-                <span class="img-wrap"><img :class="{'not-student-privilege': n === 1}" src="@/assets/lessonImg/legal_privilege.png" alt=""></span>{{$t('lesson.beShownReport')}}</p>
-              <p :class="{'not-student-privilege-text': n === 1}">
-                <span class="img-wrap"><img :class="{'not-student-privilege': n === 1}" src="@/assets/lessonImg/legal_privilege.png" alt=""></span>{{$t('lesson.trackTeachingProgress')}}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+      <learner-and-teacher class="teacher-wrap-learner-and-teacher"></learner-and-teacher>
     </div>
     <div @click.stop v-if="isLoginDialogShow">
       <login-dialog :show="isLoginDialogShow" @close="closeLoginDialog"></login-dialog>
@@ -72,6 +28,7 @@
 import { mapGetters, mapActions } from 'vuex'
 import { lesson } from '@/api'
 import LoginDialog from '@/components/common/LoginDialog'
+import LearnerAndTeacher from '@/components/lesson/common/LearnerAndTeacher'
 
 export default {
   name: 'NotActivatedTeacherRole',
@@ -104,7 +61,7 @@ export default {
       get() {
         return this.userIsLogined
       },
-      set() {}
+      set() { }
     },
     validCode() {
       return this.activeCode && this.organization
@@ -135,14 +92,14 @@ export default {
           .catch(err => {
             this.$alert(
               `<span style="color:#f75858;">` +
-                this.$t('lesson.notActivatedText.wrongCodeHint') +
-                `</span>`,
+              this.$t('lesson.notActivatedText.wrongCodeHint') +
+              `</span>`,
               '',
               {
                 confirmButtonText: this.$t('common.Sure'),
                 center: true,
                 dangerouslyUseHTMLString: true,
-                callback: action => {}
+                callback: action => { }
               }
             )
           })
@@ -155,6 +112,7 @@ export default {
     }
   },
   components: {
+    LearnerAndTeacher,
     LoginDialog
   }
 }
@@ -164,7 +122,7 @@ export default {
 .teacher-wrap {
   background: #fff;
   margin-top: 20px;
-  .lihgt{
+  .lihgt {
     color: #ff742e;
   }
   .teacher {
@@ -190,62 +148,10 @@ export default {
         }
       }
     }
-    &-acquire {
-      margin: 60px 0;
-      &-title {
-        text-align: center;
-        font-size: 24px;
-      }
-      &-box {
-        display: flex;
-        justify-content: space-between;
-        .acquire-item {
-          width: 570px;
-          margin-bottom: 15px;
-          font-size: 14px;
-          line-height: 30px;
-          box-shadow: 1px 1px 5px #ddd9d9, -1px -1px 5px #ddd9d9;
-          .role {
-            height: 100px;
-            text-align: center;
-            font-size: 24px;
-            color: #333333;
-            background: #f7f7f7;
-            &-text {
-              font-weight: bold;
-              color: #333;
-              padding-top: 20px;
-            }
-            &-cost {
-              font-size: 18px;
-              color: #10c55b;
-            }
-          }
-          .access {
-            padding: 44px 25px;
-            background: #fff;
-            p {
-              display: flex;
-              align-items: center;
-              .img-wrap {
-                margin-right: 8px;
-                display: inline-block;
-                width: 20px;
-                height: 20px;
-                .not-student-privilege {
-                  visibility: hidden;
-                }
-              }
-            }
-            .not-student-privilege-text {
-              color: rgb(179, 177, 177);
-            }
-            .teaching-function {
-              margin-left: 20px;
-            }
-          }
-        }
-      }
+  }
+  &-learner-and-teacher {
+    .acquire-item {
+      width: 570px;
     }
   }
 }
@@ -264,15 +170,6 @@ export default {
           .active-code-button {
             width: 100px;
             height: 27px;
-          }
-        }
-      }
-      &-acquire {
-        &-box {
-          display: block;
-          .acquire-item {
-            margin-bottom: 20px;
-            width: 100%;
           }
         }
       }
