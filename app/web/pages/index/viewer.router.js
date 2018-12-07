eb34ef437da451fae8ec1c83ee0bd8c9c76c5287
DDXY1230 <2502752288@qq.com>
2018-12-07 16:24:54 +0800

    add agreement

--- app/web/pages/index/viewer.router.js
+++ app/web/pages/index/viewer.router.js
@@ -4,6 +4,7 @@ const HomePage = () => import('@/components/common/HomePage')
 const CreativityPage = () => import('@/components/common/CreativityPage')
 const ExplorationPage = () => import('@/components/common/ExplorationPage')
 const StudyPage = () => import('@/components/common/StudyPage')
+const KeepworkAgreement = () => import('@/components/common/KeepworkAgreement')
 
 
 Vue.use(Router)
@@ -30,6 +31,11 @@ export default new Router({
       path: '/l',
       name: 'StudyPage',
       component: StudyPage,
+    },
+    {
+      path: '/agreement',
+      name: 'KeepworkAgreement',
+      component: KeepworkAgreement
     }
   ]
 })
