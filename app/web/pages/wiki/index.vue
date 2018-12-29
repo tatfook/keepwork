9b2519d4c6870d01374e6173bd685c363b8803f3
kevinxft <kevinxft@gmail.com>
2018-12-29 11:39:54 +0800

    fix paracrat url redirect

---
+++ app/web/pages/wiki/index.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue'
+import Vuex from 'vuex'
+import router from './wiki.router'
+
+router.beforeEach(async (to, from, next) => {
+  console.log(to)
+  if (to.name === 'redirectToPay') {
+    const { additional, app_goods_id, username } = to.query
+    let userObject = JSON.parse(additional)
+    window.location.href = `${window.location.origin}/a/orderConfirm?type=1&payment=rmb&id=${app_goods_id}&user_nid=${userObject.user_nid}&username=${username}`
+  }
+})
+
+export default {
+  router
+}
+</script>
---
+++ app/web/pages/wiki/wiki.router.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: '/wiki',
+  routes: [
+    {
+      path: '/pay',
+      name: 'redirectToPay'
+    }
+  ]
+})
