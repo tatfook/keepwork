9c5c2d4b9b3d1854c600669a8e337ca3a3f55199
onedou <onedou@126.com>
2019-01-16 14:53:57 +0800

    update ppt comp and mod

--- app/web/components/adi/common/ppt/Ppt.vue
+++ app/web/components/adi/common/ppt/Ppt.vue
@@ -1,50 +1,128 @@
 <template>
   <div class="comp comp-ppt">
-    Hello World!!!
-    <div class="reveal-t">
-      <div class="slides-t">
-        <section data-markdown data-separator="---">
-          # 主題1
-          - 主題1-內容1
-          - 主題1-內容2
-          ---
-          12312312312
-          ---
-          <!-- <vue-markdown
-            :source="'# HAHAHA\n## UUUUUUUU\n ### OOOOOO \n ```@Paracraft```'"/> -->
-        </section>
-
-        <section>Vertical Slide 1</section>
-        <section>Vertical Slide 2</section>
+    <div class="render"></div>
+    <div class="data">
+      <div class="reveal">
+        <div class="slides">
+          <section v-for="item in getData" :key="item.key">
+            <div data-template>{{ item.val }}</div>
+          </section>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import compBaseMixin from '../comp.base.mixin'
 import ppt from 'reveal.js'
+import markdown from 'reveal.js/plugin/markdown/markdown.js'
 import 'reveal.js/css/reveal.scss'
+import 'reveal.js/css/theme/source/night.scss'
+import { setTimeout } from 'timers'
 
 export default {
   name: 'AdiPpt',
+  mixins: [compBaseMixin],
   mounted() {
-    if (this.$el) {
-      const wrapper = this.$el.querySelector('.reveal-t')
-      const slides = this.$el.querySelector('.slides-t')
-      console.log(wrapper)
-      console.log(wrapper.classList)
-      console.log(slides)
+    this.updatePpt()
+  },
+  updated() {
+    this.updatePpt()
+  },
+  computed: {
+    getData() {
+      if (!this.properties.data) {
+        return []
+      }
 
-      ppt.initialize(
-        {
-          dom: {
-            wrapper: wrapper,
-            slides: slides
-          }
+      const mdArray = this.properties.data.split('---')
+      const renderArray = []
+
+      for (const index in mdArray) {
+        const val = mdArray[index]
+
+        renderArray.push({ index, val })
+      }
+
+      return renderArray
+    }
+  },
+  methods: {
+    async updatePpt() {
+      if (this.$el) {
+        const render = this.$el.querySelector('.render')
+        const data = this.$el.querySelector('.data')
+
+        if (!render || !data) {
+          return false
         }
-      )
+
+        render.innerHTML = null
+        await render.appendChild(data.firstChild.cloneNode(true))
+
+        const wrapper = this.$el.querySelectorAll('.reveal')
+        const slides = this.$el.querySelectorAll('.slides')
+
+        if (!wrapper[0] || !slides[0]) {
+          return false
+        }
+
+        wrapper[0].style.height = (wrapper[0].clientWidth / 16 * 9) + 'px'
+
+        for (const item of slides[0].childNodes) {
+          item.setAttribute('data-markdown', '')
+        }
+
+        if (!ppt.isReady()) {
+          ppt.initialize(
+            {
+              dom: {
+                wrapper: wrapper[0],
+                slides: slides[0]
+              }
+            }
+          )
+
+          this.markdown = markdown({ppt})
+        } else {
+          ppt.updateDom(
+            {
+              dom: {
+                wrapper: wrapper[0],
+                slides: slides[0]
+              }
+            }
+          )
+
+          for (const index in slides[0].childNodes) {
+            const item = slides[0].childNodes[index]
+
+            if (parseInt(index) !== 0 && typeof item === 'object') {
+              item.style.display = null
+            }
+          }
+
+          this.markdown.processSlides()
+				  this.markdown.convertSlides()
+        }
+
+        setTimeout(() => {
+          ppt.layout()
+        }, 100)
+      }
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.comp-ppt {
+  .data {
+    display: none;
+  }
+}
+</style>
+
+
 
