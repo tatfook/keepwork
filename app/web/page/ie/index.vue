89f6a0c3ceed67cb709901fac22e8f5f39aca04a
kevinxft <kevinxft@gmail.com>
2019-04-28 17:08:16 +0800

    fix auto login and low browser tips

--- app/web/page/ie/index.vue
+++ app/web/page/ie/index.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="browser-version" style="width: 450px; margin: 250px auto; font-size: 20px; color: #333;">
-    <img class="browser-version-logo" style="width:300px;display: block; margin: 0 auto;" :src="logo" alt="keepwork">
-    <div class="tips-title" style="font-size: 34px;text-align:center;margin-top:20px;font-weight: bold;">温馨提示</div>
-    <div class="margin-top-lg" style="margin-top: 60px;">您的浏览器版本过低，为保证正常显示，请升级。</div>
-    <div>推荐：</div>
-    <div style="margin-top: 30px;">Google Chrome <a class="black-link" style="color: #333;text-decoration:none;" href="https://www.google.cn/chrome/">[打开官网]</a> </div>
+  <div :style="backgroundStyle">
+    <div class="browser-version" style="width: 450px; text-align: center; margin: 0 auto; padding-top:250px; font-size: 20px; color: #333;">
+      <img class="browser-version-logo" style="width:300px;display: block; margin: 0 auto;" :src="logo" alt="keepwork">
+      <div class="tips-title" style="font-size: 28px;margin-top:20px;font-weight: bold; display: inline-block;">温馨提示</div>
+      <div class="margin-top-lg" style="font-size: 18px;margin-top: 50px;">您的浏览器版本过低，为保证正常显示，请升级。</div>
+      <div style="font-size: 18px; margin-top: 20px;">推荐： <a style="color: #409eff" href="https://www.google.cn/chrome/">Google Chrome[打开官网]</a> </div>
+    </div>
   </div>
 </template>
 
@@ -14,7 +15,13 @@ export default {
   name: 'ie',
   data() {
     return {
-      logo: require('@/assets/img/logo_old.png')
+      logo: require('@/assets/img/logo_old.png'),
+      bg: require('@/assets/img/old_browser.jpg')
+    }
+  },
+  computed: {
+    backgroundStyle() {
+      return `width: 1920px; height: 1080px; margin: 0 auto;background: url('${this.bg}') no-repeat center;`
     }
   }
 }
