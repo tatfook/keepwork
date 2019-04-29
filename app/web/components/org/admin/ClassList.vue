c119ad5800652da00a6056261fd9a70dbe217ca4
DDXY1230 <2502752288@qq.com>
2019-04-29 18:28:19 +0800

    fix

--- app/web/components/org/admin/ClassList.vue
+++ app/web/components/org/admin/ClassList.vue
@@ -10,7 +10,7 @@
       <el-table-column prop="name" :label="$t('org.ClassNameLabel')" width="240">
       </el-table-column>
       <el-table-column :label="$t('org.beginClassTime')" width="240"><template slot-scope="scope">{{scope.row.begin | formatTime}} - {{scope.row.end | formatTime}}</template></el-table-column>
-      <el-table-column :label="$t('org.LessonPackagesAvailable')">
+      <el-table-column :label="$t('common.action')">
         <template slot-scope="scope">
           <router-link class='class-list-table-link' :to='{name: "OrgClassDetail", query: scope.row}'>{{$t('org.Details')}}</router-link>
           <router-link class='class-list-table-link' :to='{name: "OrgEditClass", query: scope.row}'>{{$t('org.Edit')}}</router-link>
