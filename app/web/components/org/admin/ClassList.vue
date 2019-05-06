0184bb10f15a0e225ff2d94024b269c219bfd93e
ZhangKaitlyn <1873070425@qq.com>
2019-05-06 10:59:31 +0800

    显示未开班的班级

--- app/web/components/org/admin/ClassList.vue
+++ app/web/components/org/admin/ClassList.vue
@@ -46,7 +46,7 @@ export default {
       return _.filter(this.orgClassesWithOvertime, classDetail => {
         let classBegin = new Date(classDetail.begin).valueOf()
         let classEnd = new Date(classDetail.end).valueOf()
-        return classBegin <= nowDate && classEnd >= nowDate
+        return classEnd >= nowDate
       })
     },
     orgClassesLength() {
