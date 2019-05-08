6ef010a28a51758d64b1f40f31dba9b140dcff61
DDXY1230 <2502752288@qq.com>
2019-05-08 16:34:49 +0800

    fix format

--- app/web/components/org/admin/OrgHistoryEditClass.vue
+++ app/web/components/org/admin/OrgHistoryEditClass.vue
@@ -7,59 +7,59 @@
 import HistoryClassComp from './common/HistoryClassComp'
 import { mapActions, mapGetters } from 'vuex'
 export default {
-	name: 'OrgHistoryEditClass',
-	data() {
-		return {
-			isLoading: false
-		}
-	},
-	computed: {
-		...mapGetters({
-			currentOrg: 'org/currentOrg'
-		}),
-		orgId() {
-			return _.get(this.currentOrg, 'id')
-		},
-		classDetail() {
-			return _.get(this.$route, 'query')
-		}
-	},
-	methods: {
-		...mapActions({
-			orgUpdateClass: 'org/updateClass'
-		}),
-		toHistoryClassListPage() {
-			this.$router.push({
-				name: 'HistoricalData'
-			})
-		},
-		async updateClass(updatedClassData) {
-			this.isLoading = true
-			await this.orgUpdateClass({
-				organizationId: this.orgId,
-				classId: this.classDetail.id,
-				...updatedClassData
-			})
-				.then(() => {
-					this.$message({
-						message: this.$t('org.successfullyUpdatedClass'),
-						type: 'success'
-					})
-					this.isLoading = false
-					this.toHistoryClassListPage()
-				})
-				.catch(error => {
-					console.log(error)
-					this.$message({
-						message: this.$t('org.failedUpdatedClass'),
-						type: 'error'
-					})
-					this.isLoading = false
-				})
-		}
-	},
-	components: {
-		HistoryClassComp
-	}
+  name: 'OrgHistoryEditClass',
+  data() {
+    return {
+      isLoading: false
+    }
+  },
+  computed: {
+    ...mapGetters({
+      currentOrg: 'org/currentOrg'
+    }),
+    orgId() {
+      return _.get(this.currentOrg, 'id')
+    },
+    classDetail() {
+      return _.get(this.$route, 'query')
+    }
+  },
+  methods: {
+    ...mapActions({
+      orgUpdateClass: 'org/updateClass'
+    }),
+    toHistoryClassListPage() {
+      this.$router.push({
+        name: 'HistoricalData'
+      })
+    },
+    async updateClass(updatedClassData) {
+      this.isLoading = true
+      await this.orgUpdateClass({
+        organizationId: this.orgId,
+        classId: this.classDetail.id,
+        ...updatedClassData
+      })
+        .then(() => {
+          this.$message({
+            message: this.$t('org.successfullyUpdatedClass'),
+            type: 'success'
+          })
+          this.isLoading = false
+          this.toHistoryClassListPage()
+        })
+        .catch(error => {
+          console.log(error)
+          this.$message({
+            message: this.$t('org.failedUpdatedClass'),
+            type: 'error'
+          })
+          this.isLoading = false
+        })
+    }
+  },
+  components: {
+    HistoryClassComp
+  }
 }
 </script>
