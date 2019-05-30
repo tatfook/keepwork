3c2843cf470fdd46531055159b83cb66523087e1
DDXY1230 <32215445+DDXY1230@users.noreply.github.com>
2019-05-30 16:15:43 +0800

    Update HistoricalData.vue

--- app/web/components/org/admin/HistoricalData.vue
+++ app/web/components/org/admin/HistoricalData.vue
@@ -68,7 +68,7 @@ export default {
     }),
     handleSizeChange(val) {
       this.perPage = val
-       this.targetPage(1)
+      this.targetPage(1)
     },
     targetPage(targetPage) {
       this.page = targetPage
