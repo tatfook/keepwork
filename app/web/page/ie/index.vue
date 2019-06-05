76ed5263eca9a760ddb498b69914d21e6ff8b0db
kevinxft <kevinxft@gmail.com>
2019-06-05 14:18:58 +0800

    fix 4715

--- app/web/page/ie/index.vue
+++ app/web/page/ie/index.vue
@@ -4,7 +4,9 @@
       <img class="browser-version-logo" style="width:300px;display: block; margin: 0 auto;" :src="logo" alt="keepwork">
       <div class="tips-title" style="font-size: 28px;margin-top:20px;font-weight: bold; display: inline-block;">温馨提示</div>
       <div class="margin-top-lg" style="font-size: 18px;margin-top: 50px;">您的浏览器版本过低，为保证正常显示，请升级。</div>
-      <div style="font-size: 18px; margin-top: 20px;">推荐： <a style="color: #409eff" href="https://www.google.cn/chrome/">Google Chrome[打开官网]</a> </div>
+      <div style="font-size: 18px; margin-top: 20px;">推荐： <a style="color: #409eff" target="_blank" href="https://www.google.cn/chrome/">Google Chrome[打开官网]</a> </div>
+      <div style="font-size: 18px; margin-top: 20px;">XP用户推荐： <a style="color: #409eff" target="_blank" href="http://paracraft.keepwork.com/download?lang=zh">Chrome浏览器 WinXP版本</a></div>
+      <div style="font-size: 14px; margin-top: 10px;margin-right: -100px;">点击最下方"其他下载"进行下载</div>
     </div>
   </div>
 </template>
@@ -21,7 +23,7 @@ export default {
   },
   computed: {
     backgroundStyle() {
-      return `width: 1920px; height: 1080px; margin: 0 auto;background: url('${this.bg}') no-repeat center;`
+      return `height: 1080px; margin: 0 auto;background: url('${this.bg}') no-repeat center;`
     }
   }
 }
