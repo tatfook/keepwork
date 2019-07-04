1f1f9de67c4195edf205efb3bc7247e828041a03
ZhangKaitlyn <1873070425@qq.com>
2019-07-04 13:56:17 +0800

    test

--- app/web/components/paracraft/common/CompItem.vue
+++ app/web/components/paracraft/common/CompItem.vue
@@ -18,6 +18,9 @@
 </template>
 
 <script>
+const THREE = require('three')
+window.THREE = THREE
+require('three/examples/js/loaders/GLTFLoader')
 export default {
   name: 'CompItem',
   props: {
@@ -25,6 +28,9 @@ export default {
       type: Object,
       required: true
     }
+  },
+  mounted() {
+    this.renderCompCover()
   },
   computed: {
     downloadUrl() {
@@ -34,7 +40,123 @@ export default {
   methods: {
     useComp() {
       alert(this.downloadUrl)
+    },
+    renderCompCover() {
+      var scene = new THREE.Scene()
+      var camera = new THREE.PerspectiveCamera(
+        80,
+        window.innerWidth / window.innerHeight,
+        0.01,
+        20000
+      )
+      // camera.position.set(0, 0, 0)
+      camera.position.set(1, 0, 1)
+
+      var light = new THREE.PointLight(0xffffcc, 20, 200)
+      light.position.set(4, 30, -20)
+      scene.add(light)
+
+      var light2 = new THREE.AmbientLight(0x20202a, 20, 100)
+      light2.position.set(30, -10, 30)
+      scene.add(light2)
+
+      var renderer = new THREE.WebGLRenderer()
+      renderer.setSize(window.innerWidth, window.innerHeight)
+      document.body.appendChild(renderer.domElement)
+
+      var loader = new THREE.GLTFLoader()
+      loader.load(
+        'https://api.keepwork.com/storage/v0/siteFiles/4389/raw#Duck.glb',
+        function(gltf) {
+          console.log(gltf)
+          var object = gltf.scene
+          // gltf.scene.scale.set(2, 2, 2)
+          // gltf.scene.position.x = 0 //Position (x = right+ left-)
+          // gltf.scene.position.y = 0 //Position (y = up+, down-)
+          // gltf.scene.position.z = 0 //Position (z = front +, back-)
+          scene.add(gltf.scene)
+        },
+        undefined,
+        function(error) {
+          console.error(error)
+        }
+      )
+
+      // renderer.render(scene, camera)
+      function render() {
+        requestAnimationFrame(render)
+        renderer.render(scene, camera)
+      }
+      render()
     }
+    // renderCompCover() {
+    //   // const backgroundColor = 0x000000
+    //   var renderCalls = []
+    //   function render() {
+    //     requestAnimationFrame(render)
+    //     renderCalls.forEach(callback => {
+    //       callback()
+    //     })
+    //   }
+    //   render()
+
+    //   var scene = new THREE.Scene()
+    //   var camera = new THREE.PerspectiveCamera(
+    //     200,
+    //     window.innerWidth / window.innerHeight,
+    //     0.1,
+    //     2000
+    //   )
+    //   camera.position.set(1, 0, 1)
+
+    //   var renderer = new THREE.WebGLRenderer({ antialias: true })
+    //   renderer.setPixelRatio(window.devicePixelRatio)
+    //   renderer.setSize(window.innerWidth, window.innerHeight)
+    //   // renderer.setClearColor(backgroundColor) //0x );
+
+    //   renderer.toneMapping = THREE.LinearToneMapping
+    //   renderer.toneMappingExposure = Math.pow(0.94, 5.0)
+    //   renderer.shadowMap.enabled = true
+    //   renderer.shadowMap.type = THREE.PCFShadowMap
+
+    //   window.addEventListener(
+    //     'resize',
+    //     function() {
+    //       camera.aspect = window.innerWidth / window.innerHeight
+    //       camera.updateProjectionMatrix()
+    //       renderer.setSize(window.innerWidth, window.innerHeight)
+    //     },
+    //     false
+    //   )
+
+    //   document.body.appendChild(renderer.domElement)
+
+    //   function renderScene() {
+    //     renderer.render(scene, camera)
+    //   }
+    //   renderCalls.push(renderScene)
+    //   var light = new THREE.PointLight(0xffffcc, 20, 200)
+    //   light.position.set(4, 30, -20)
+    //   scene.add(light)
+
+    //   var light2 = new THREE.AmbientLight(0x20202a, 20, 100)
+    //   light2.position.set(30, -10, 30)
+    //   scene.add(light2)
+
+    //   /* ////////////////////////////////////////////////////////////////////////// */
+
+    //   var loader = new THREE.GLTFLoader()
+    //   loader.crossOrigin = true
+    //   loader.load(
+    //     'https://api.keepwork.com/storage/v0/siteFiles/4389/raw#Duck.glb',
+    //     // 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/39255/ladybug.gltf',
+    //     function(data) {
+    //       var object = data.scene
+    //       // object.position.set(5, -5, -0.75)
+    //       scene.add(object)
+    //     }
+    //   )
+    // }
   }
 }
 </script>
