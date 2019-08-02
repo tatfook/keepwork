b499dc3831bd7e500fbe27d742577849cd3c274f
ZhangKaitlyn <1873070425@qq.com>
2019-08-02 16:21:49 +0800

    二维码

--- app/web/components/org/OrgFormDetail.vue
+++ app/web/components/org/OrgFormDetail.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="org-form-detail" v-loading="isLoading">
     <org-header></org-header>
-    <form-preview class="org-form-detail-form" :type="type" :title="title" :description="description" :text="text" :quizzes="quizzes" :isAnswerMode="true"></form-preview>
+    <div class="org-form-detail-container">
+      <form-preview class="org-form-detail-form" :type="type" :title="title" :description="description" :text="text" :quizzes="quizzes" :isAnswerMode="true"></form-preview>
+      <div class="org-form-detail-qrcode">
+        <div class="org-form-detail-qrcode-main">
+          <qrcode-vue :value="nowPageLink" :size="60" level="L" className="org-form-detail-qrcode-box"></qrcode-vue>
+        </div>
+        扫一扫，手机继续看
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import QrcodeVue from 'qrcode.vue'
 import OrgHeader from './common/OrgHeader'
 import FormPreview from './common/FormPreview'
 import { mapGetters, mapActions } from 'vuex'
@@ -19,6 +28,9 @@ export default {
     ...mapGetters({
       getFormDetailById: 'org/getFormDetailById'
     }),
+    nowPageLink() {
+      return window.location.href
+    },
     formId() {
       return _.get(this.$route, 'params.id')
     },
@@ -52,6 +64,7 @@ export default {
     })
   },
   components: {
+    QrcodeVue,
     OrgHeader,
     FormPreview
   }
@@ -62,6 +75,38 @@ export default {
   &-form {
     margin-top: 20px;
     margin-bottom: 20px;
+  }
+  &-container {
+    width: 1200px;
+    max-width: 100%;
+    margin: 0 auto;
+    position: relative;
+    display: table-cell;
+  }
+  &-qrcode {
+    position: absolute;
+    right: 0;
+    top: 20px;
+    text-align: center;
+    font-size: 12px;
+    &-main {
+      margin-bottom: 8px;
+    }
+    &-box {
+      display: inline-block;
+      padding: 6px;
+      background-color: #fff;
+      border-radius: 8px;
+    }
+  }
+}
+</style>
+<style lang="scss" scoped>
+@media screen and (max-width: 1016px) {
+  .org-form-detail {
+    &-qrcode {
+      display: none;
+    }
   }
 }
 </style>
