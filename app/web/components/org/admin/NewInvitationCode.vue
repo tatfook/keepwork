4c4910b0f78aefd3f44e2adb15ed8422243ca0ec
kevinxft <kevinxft@gmail.com>
2019-08-19 15:32:05 +0800

    finish 5082

--- app/web/components/org/admin/NewInvitationCode.vue
+++ app/web/components/org/admin/NewInvitationCode.vue
@@ -9,7 +9,7 @@
       </div>
       <div class="new-invitation-code-top-operation">
         <el-button class="new-invitation-code-top-operation-button new-invitation-code-top-operation-button-export" @click="cancelCreateActiveCode()">{{$t('common.Cancel')}}</el-button>
-        <el-button :disabled="disabledCreate" type="primary" class="new-invitation-code-top-operation-button" @click="createActiveCode()">{{$t('common.Sure')}}</el-button>
+        <el-button :disabled="disabledCreate" type="primary" class="new-invitation-code-top-operation-button" :loading="isLoading" @click="createActiveCode()">{{$t('common.Sure')}}</el-button>
       </div>
     </div>
     <div class="new-invitation-code-content">
@@ -19,15 +19,15 @@
             <el-option v-for="(classItem, index) in orgClassesFilter" :key="index" :label="classItem.name" :value="classItem.id"></el-option>
           </el-select>
         </el-form-item>
-        <el-radio class="new-invitation-code-content-radio" v-model="radioValue" label="1">
+        <el-radio class="new-invitation-code-content-radio" v-model="radioValue" label="byCount">
           <el-form-item label="生成指定数量" prop="quantity">
-            <el-input type="number" placeholder="1 ~ 100之间" :disabled="radioValue !== '1'" v-model="codeAssociateInfo.quantity" size="medium"></el-input>
+            <el-input type="number" placeholder="1 ~ 100之间" :disabled="radioValue !== 'byCount'" v-model="codeAssociateInfo.quantity" size="medium"></el-input>
           </el-form-item>
         </el-radio>
         <div></div>
-        <el-radio class="new-invitation-code-content-radio" v-model="radioValue" label="2">
+        <el-radio class="new-invitation-code-content-radio" v-model="radioValue" label="byName">
           <el-form-item label="输入学生姓名" prop="studentNames">
-            <el-input class="new-invitation-code-content-textarea" placeholder="每行一位" type="textarea" :autosize="{ minRows: 3, maxRows: 20 }" :disabled="radioValue !== '2'" v-model="codeAssociateInfo.studentNames" size="medium"></el-input>
+            <el-input class="new-invitation-code-content-textarea" placeholder="每行一位" type="textarea" :autosize="{ minRows: 3, maxRows: 20 }" :disabled="radioValue !== 'byName'" v-model="codeAssociateInfo.studentNames" size="medium"></el-input>
           </el-form-item>
         </el-radio>
       </el-form>
@@ -41,19 +41,20 @@ export default {
   name: 'NewInvitationCode',
   data() {
     const checkQuantity = (rule, value, callback) => {
-      if (this.radioValue === '1' && !value) {
+      if (this.radioValue === 'byCount' && !value) {
         return callback(new Error('请填写生成数量'))
       }
     }
 
     const checkStudentName = (rule, value, callback) => {
-      if (this.radioValue === '2' && !value.trim()) {
+      if (this.radioValue === 'byName' && !value.trim()) {
         callback(new Error('请填写学生姓名'))
       }
     }
 
     return {
-      radioValue: '1',
+      radioValue: 'byCount',
+      isLoading: false,
       codeAssociateInfo: {
         quantity: '',
         classId: '',
@@ -65,18 +66,21 @@ export default {
       }
     }
   },
+  async mounted() {
+    await this.getCurrentOrgClassList()
+  },
   computed: {
     ...mapGetters({
       currentOrg: 'org/currentOrg',
       getOrgClassesById: 'org/getOrgClassesById'
     }),
     disabledCreate() {
-      if (this.radioValue === '1') {
+      if (this.radioValue === 'byCount') {
         return (
           !this.codeAssociateInfo.quantity || !this.codeAssociateInfo.classId
         )
       }
-      if (this.radioValue === '2') {
+      if (this.radioValue === 'byName') {
         return (
           !this.codeAssociateInfo.studentNames.trim() ||
           !this.codeAssociateInfo.classId
@@ -97,16 +101,41 @@ export default {
   },
   methods: {
     ...mapActions({
-      createBatchCode: 'org/createBatchCode'
+      createBatchCode: 'org/createBatchCode',
+      getCurrentOrgClassList: 'org/getCurrentOrgClassList'
     }),
     cancelCreateActiveCode() {
       this.$router.push({ name: 'InvitationCode' })
     },
     async createActiveCode() {
-      let codeList = await this.createBatchCode({
-        count: this.codeAssociateInfo.quantity,
-        classId: this.codeAssociateInfo.classId
-      })
+      this.isLoading = true
+      if (this.radioValue === 'byCount') {
+        await this.createBatchCode({
+          count: this.codeAssociateInfo.quantity,
+          classId: this.codeAssociateInfo.classId
+        }).catch(e => {
+          this.$message.error('创建失败')
+          this.isLoading = false
+        })
+      }
+      if (this.radioValue === 'byName') {
+        const names = this.codeAssociateInfo.studentNames
+          .split('\n')
+          .filter(v => v)
+        if (names.length > 100) {
+          this.$message.error('一次最多只能生成100个学生的邀请码')
+          this.isLoading = false
+          return
+        }
+        await this.createBatchCode({
+          count: names.length,
+          classId: this.codeAssociateInfo.classId,
+          names
+        }).catch(e => {
+          this.$message.error('创建失败')
+          this.isLoading = false
+        })
+      }
       let currentClass = this.orgClasses.find(i => {
         return i.id === this.codeAssociateInfo.classId
       })
