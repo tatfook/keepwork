d94a0fd1d8260a5e2910fe896c5e7b9fc2872e81
kevinxft <kevinxft@gmail.com>
2019-10-09 16:33:42 +0800

    fix some bug

--- app/web/components/org/admin/PrintInvitationCode.vue
+++ app/web/components/org/admin/PrintInvitationCode.vue
@@ -54,9 +54,10 @@
               <img class="print-invitation-code-print-content-box-top-right" src="@/assets/org/shining.png" alt="">
             </div>
             <div class="print-invitation-code-print-content-box-guide">
-              <p class="print-invitation-code-print-content-box-guide-step"><span class="print-invitation-code-print-content-box-guide-step-num">1</span>用浏览器打开网址：keepwork.com/org/{{orgLoginUrl}}</p>
-              <p class="print-invitation-code-print-content-box-guide-step"><span class="print-invitation-code-print-content-box-guide-step-num">2</span>注册keepwork账号，并登录</p>
-              <p class="print-invitation-code-print-content-box-guide-step"><span class="print-invitation-code-print-content-box-guide-step-num">3</span>输入上方邀请码</p>
+              <p class="print-invitation-code-print-content-box-guide-step"><span class="print-invitation-code-print-content-box-guide-step-num">1</span>
+                打开Paracraft客户端（下载地址：paracraft.keepwork.com） </p>
+              <p class="print-invitation-code-print-content-box-guide-step"><span class="print-invitation-code-print-content-box-guide-step-num">2</span>注册账号，并登录Paracraft客户端</p>
+              <p class="print-invitation-code-print-content-box-guide-step"><span class="print-invitation-code-print-content-box-guide-step-num">3</span>进入“我的学校”，输入上方邀请码</p>
               <img class="print-invitation-code-print-content-box-guide-img" src="@/assets/org/para-icon.png" alt="">
             </div>
           </div>
@@ -68,6 +69,7 @@
 <script>
 import { mapGetters } from 'vuex'
 import moment from 'moment'
+import _ from 'lodash'
 
 export default {
   name: 'PrintInvitationCode',
@@ -101,7 +103,7 @@ export default {
       return _.chunk(this.printCodeListData, 2)
     },
     className() {
-      return _.get(this.$route.query.className, '')
+      return this.$route.query.className || ''
     }
   },
   methods: {
