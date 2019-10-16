240c74f496344358fa693ac98ac48a3078e2ee39
kevinxft <kevinxft@gmail.com>
2019-10-16 15:18:32 +0800

    add preview

--- app/web/components/org/teacher/ReportType.vue
+++ app/web/components/org/teacher/ReportType.vue
@@ -19,10 +19,13 @@
           <i v-show="item.id === selectedType" class="el-icon-success type-choice-item-main-checked-icon"></i>
         </div>
         <div class="type-choice-item-preview">
-          <span>预览</span>
+          <span @click="handleShowPreview(item.preview)">预览</span>
         </div>
       </div>
     </div>
+    <el-dialog top="2vh" custom-class="report-type-preview" :visible.sync="isShowPreview">
+      <img class="report-type-preview-image" :src="previewUrl" alt="预览图">
+    </el-dialog>
   </div>
 </template>
 
@@ -39,12 +42,20 @@ export default {
   data() {
     return {
       smallEvaluationLogo: require('@/assets/org/teacher/small_evaluation.png'),
-      stageSummaryLogo: require('@/assets/org/teacher/stage_summary.png')
+      stageSummaryLogo: require('@/assets/org/teacher/stage_summary.png'),
+      smallEvaluationPreview: require('@/assets/org/teacher/small_evaluation_preview.png'),
+      stageSummaryPreview: require('@/assets/org/teacher/stage_summary_preview.png'),
+      isShowPreview: false,
+      previewUrl: ''
     }
   },
   methods: {
     hanldeSelectType(id) {
       this.$emit('change', id)
+    },
+    handleShowPreview(url) {
+      this.previewUrl = url
+      this.isShowPreview = true
     }
   },
   computed: {
@@ -54,14 +65,16 @@ export default {
           id: 1,
           logo: this.smallEvaluationLogo,
           title: '小评',
-          tips: '报告中，仅包含本次的点评内容'
+          tips: '报告中，仅包含本次的点评内容',
+          preview: this.smallEvaluationPreview
         },
         {
           id: 2,
           logo: this.stageSummaryLogo,
           title: '阶段总结',
           tips:
-            '报告中，除了本次的点评内容外，还将附上对学生历次点评的统计分析（由系统自动生成）'
+            '报告中，除了本次的点评内容外，还将附上对学生历次点评的统计分析（由系统自动生成）',
+          preview: this.stageSummaryPreview
         }
       ]
     }
@@ -130,6 +143,24 @@ export default {
         font-size: 14px;
         color: #2397f3;
       }
+    }
+  }
+
+  /deep/ &-preview {
+    max-width: 630px;
+    .el-dialog__header {
+      position: relative;
+      padding: 0;
+      .el-dialog__headerbtn {
+        position: absolute;
+        margin-bottom: -20px;
+      }
+    }
+    .el-dialog__body {
+      padding: 0;
+    }
+    &-image {
+      width: 100%;
     }
   }
 }
