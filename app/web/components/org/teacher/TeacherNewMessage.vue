0f87a448d0d4c556e47476c79234df8e861bdb1b
ZhangKaitlyn <1873070425@qq.com>
2019-11-22 16:51:03 +0800

    提取教师和管理员端公共发消息

--- app/web/components/org/teacher/TeacherNewMessage.vue
+++ app/web/components/org/teacher/TeacherNewMessage.vue
@@ -1,38 +1,15 @@
 <template>
-  <new-message v-loading="isLoading" @cancel="back" @save="saveNewMessage"></new-message>
+  <new-message @cancel="back" @back="back"></new-message>
 </template>
 <script>
 import NewMessage from '@/components/org/common/NewMessage'
-import { mapActions } from 'vuex'
 export default {
   name: 'TeacherNewMessage',
-  data() {
-    return {
-      isLoading: false,
-    }
-  },
   methods: {
-    ...mapActions({
-      orgCreateNewMessage: 'org/createNewMessage',
-    }),
     back() {
       this.$router.push({
         name: 'TeacherMessageList',
       })
-    },
-    async saveNewMessage(newMessage) {
-      this.isLoading = true
-      try {
-        await this.orgCreateNewMessage(newMessage)
-        this.showMessage('success', '消息发送成功')
-        this.back()
-      } catch (error) {
-        this.showMessage('danger', '消息发送失败，请重试')
-      }
-      this.isLoading = false
-    },
-    showMessage(type, message) {
-      this.$message({ type, message })
     },
   },
   components: {
