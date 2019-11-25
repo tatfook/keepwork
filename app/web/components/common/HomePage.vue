1297b5f0eb7616226701227d56e285b7a8c6e45f
ZhangKaitlyn <1873070425@qq.com>
2019-11-25 17:02:39 +0800

    格式化

--- app/web/components/common/HomePage.vue
+++ app/web/components/common/HomePage.vue
@@ -141,55 +141,53 @@ export default {
       briefPic: [
         {
           image: require('@/assets/pblImg/game0.png'),
-          text: this.$t('home.create3DGameAndAnim')
+          text: this.$t('home.create3DGameAndAnim'),
         },
         {
           image: require('@/assets/pblImg/game1.png'),
-          text: this.$t('home.selfLearning')
+          text: this.$t('home.selfLearning'),
         },
         {
           image: require('@/assets/pblImg/game2.png'),
-          text: this.$t('home.personalKnowledge')
+          text: this.$t('home.personalKnowledge'),
         },
         {
           image: require('@/assets/pblImg/game3.png'),
-          text: this.$t('home.haveYourOwnSite')
+          text: this.$t('home.haveYourOwnSite'),
         },
         {
           image: require('@/assets/pblImg/game4.png'),
-          text: this.$t('home.learnBasedOnProjects')
+          text: this.$t('home.learnBasedOnProjects'),
         },
         {
           image: require('@/assets/pblImg/game5.png'),
-          text: this.$t('home.learnFromProfessionalProgrammers')
-        }
+          text: this.$t('home.learnFromProfessionalProgrammers'),
+        },
       ],
       boardImgUrl: require('@/assets/pblImg/game0.png'),
       newsHtml: '',
       originHandpickProjects: [],
-      originLikesProjects: []
+      originLikesProjects: [],
     }
   },
   components: {
     ProjectCell,
     RegisterDialog,
     LessonPackageCell,
-    videoPlayer
+    videoPlayer,
   },
   async mounted() {
     this.textAnimation()
-    let [handpick, likes, news] = await Promise.all([
-      this.getHandpick(),
-      this.getLikes(),
-      this.getNews()
-    ]).catch(e => console.error(e))
+    let [handpick, likes, news] = await Promise.all([this.getHandpick(), this.getLikes(), this.getNews()]).catch(e =>
+      console.error(e),
+    )
     this.newsHtml = news
     this.originHandpickProjects = handpick
     this.originLikesProjects = likes
   },
   computed: {
     ...mapGetters({
-      isLogined: 'user/isLogined'
+      isLogined: 'user/isLogined',
     }),
     isEn() {
       return locale === 'en-US'
@@ -199,14 +197,14 @@ export default {
     },
     likesProjects() {
       return _.get(this.originLikesProjects, 'rows', [])
-    }
+    },
   },
   methods: {
     async getHandpick() {
       return keepwork.projects.getProjects({
         'x-order': 'choicenessNo-desc',
         'x-per-page': 8,
-        'x-page': 1
+        'x-page': 1,
       })
     },
     async getLikes() {
@@ -214,9 +212,9 @@ export default {
         'x-order': 'lastStar-desc-star-desc-updatedAt-desc',
         'x-per-page': 8,
         'x-page': 1,
-        'star':{
-          "$gt":2
-        }
+        star: {
+          $gt: 2,
+        },
       })
     },
     getPackageSuitableAge(lessonPackage) {
@@ -239,20 +237,13 @@ export default {
     combinedPic(item, len, n, leave) {
       clearInterval(this.timer_pic[n])
       this.timer_pic[n] = setInterval(() => {
-        let backgroundLen = Number(
-          this.$refs[`${item}_box_img`].style['backgroundPositionX'].replace(
-            /px/,
-            ''
-          )
-        )
+        let backgroundLen = Number(this.$refs[`${item}_box_img`].style['backgroundPositionX'].replace(/px/, ''))
         if (backgroundLen > len) {
           backgroundLen -= 100
-          this.$refs[`${item}_box_img`].style['backgroundPositionX'] =
-            backgroundLen + 'px'
+          this.$refs[`${item}_box_img`].style['backgroundPositionX'] = backgroundLen + 'px'
         } else {
           clearInterval(this.timer_pic[n])
-          if (leave)
-            this.$refs[`${item}_box_img`].style['backgroundPositionX'] = '0px'
+          if (leave) this.$refs[`${item}_box_img`].style['backgroundPositionX'] = '0px'
         }
       }, 50)
     },
@@ -268,8 +259,8 @@ export default {
       this.$router.push({
         name: 'ExplorationPage',
         query: {
-          tab: tabName
-        }
+          tab: tabName,
+        },
       })
     },
     goJoin() {
@@ -299,20 +290,16 @@ export default {
       const HomePageInfo = {
         apiPrefix: 'https://api.keepwork.com/git/v0',
         projectName: 'official/keepwork',
-        newsPath: this.isEn
-          ? 'official/keepwork/news_en.md'
-          : 'official/keepwork/news.md'
+        newsPath: this.isEn ? 'official/keepwork/news_en.md' : 'official/keepwork/news.md',
       }
-      return gitlabShowRawForGuest(
-        HomePageInfo.apiPrefix,
-        HomePageInfo.projectName,
-        HomePageInfo.newsPath
-      ).catch(e => console.error(e))
-    }
+      return gitlabShowRawForGuest(HomePageInfo.apiPrefix, HomePageInfo.projectName, HomePageInfo.newsPath).catch(e =>
+        console.error(e),
+      )
+    },
   },
   beforeDestroy() {
     clearInterval(this.subtitleAnimation)
-  }
+  },
 }
 </script>
 
@@ -406,8 +393,7 @@ export default {
         flex: 1;
         height: 442px;
         position: relative;
-        background: url('../../assets/img/homepage_mainpic.png') no-repeat
-          bottom right;
+        background: url('../../assets/img/homepage_mainpic.png') no-repeat bottom right;
         &-desc {
           position: absolute;
           .title {
