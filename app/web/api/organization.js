480a378f27f42739e05c86e60326369c1585e787
ZhangKaitlyn <1873070425@qq.com>
2019-12-13 15:07:41 +0800

    生成邀请码

--- app/web/api/organization.js
+++ app/web/api/organization.js
@@ -135,6 +135,10 @@ export const messages = {
   getMessageIndex: async params => get('userMessages/indexOfMessage', { params }),
 }
 
+export const lessonOrganizationActivateCodes = {
+  getUsedStatus: async () => get('lessonOrganizationActivateCodes/usedStatus')
+}
+
 export default {
   organizations,
   lessonOrganizations,
@@ -144,4 +148,5 @@ export default {
   users,
   evaluationReports,
   messages,
+  lessonOrganizationActivateCodes,
 }
