69b858c4a2bdba2eb82bfd2695330aebfa9d88a1
ZhangKaitlyn <1873070425@qq.com>
2020-01-08 14:44:56 +0800

    :fire: remove han contest entrance

--- app/web/components/common/HomePage.vue
+++ app/web/components/common/HomePage.vue
@@ -72,11 +72,6 @@
         </div>
       </div>
     </div>
-    <div class="home-page-notice">
-      <a href="/han" class="home-page-notice-link">
-        <img class="home-page-notice-link-img" src="@/assets/contests/han_banner.png" alt="">
-      </a>
-    </div>
     <div class="home-page-cabinet">
       <div class="home-page-cabinet-excellent selected">
         <div class="title">
@@ -305,19 +300,6 @@ export default {
 
 <style lang="scss">
 .home-page {
-  &-notice {
-    max-width: 1200px;
-    margin: 20px auto;
-    cursor: pointer;
-    &-link {
-      display: block;
-      box-sizing: border-box;
-      width: 100%;
-      &-img {
-        width: 100%;
-      }
-    }
-  }
   &-register-dialog {
     .el-dialog {
       width: 352px;
