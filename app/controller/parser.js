40a645a53f6785991c5396cd450dd0490fd9437f
leopoldchen <placeintime@live.cn>
2020-01-11 15:56:55 +0800

    fix build content api

--- app/controller/parser.js
+++ app/controller/parser.js
@@ -1,6 +1,6 @@
 'use strict'
 const { getSearchableContent } = require('../web/lib/mod/parser/mdSearchedParser/index')
-const { buildBlockList, buildMarkdown } = require('../web/lib/mod/parser/index')
+const { buildBlockList, buildMarkdown, BlockHelper } = require('../web/lib/mod/parser/index')
 module.exports = app => {
   return class ParserController extends app.Controller {
     async parser() {
@@ -22,6 +22,9 @@ module.exports = app => {
     async buildContent() {
       const { ctx } = this
       const { blocks } = ctx.request.body
+      for (let i = 0; i < blocks.length; i++) {
+        BlockHelper.buildMarkdown(blocks[i])
+      }
       ctx.body = {
         content: buildMarkdown(blocks)
       }
